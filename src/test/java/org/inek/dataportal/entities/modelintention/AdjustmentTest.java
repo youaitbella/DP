1beb6c4e592081e31f818e08d4307250c7e47b87
Stephan Schwarz <stephan.schwarz@inek-drg.de>
2014-05-27 13:58:06 +0000

    somw additional unit tests

--- src/test/java/org/inek/dataportal/entities/modelintention/AdjustmentTest.java
+++ src/test/java/org/inek/dataportal/entities/modelintention/AdjustmentTest.java
@@ -150,4 +150,53 @@ public class AdjustmentTest {
         other.setDateFrom(date);
         assertFalse(instance.equals(other));
     }
+    
+    @Test
+    public void testObjectsWithDifferentIdsShallHaveDifferentHash() {
+        System.out.println("testObjectsWithDifferentIdsShallBeTreatedAsDifferent");
+        Date date = java.util.Calendar.getInstance().getTime();
+        Adjustment instance = new Adjustment();
+        instance.setId(1);
+        instance.setAdjustmentTypeId(1);
+        instance.setDateFrom(date);
+        Adjustment other = new Adjustment();
+        other.setId(2);
+        other.setAdjustmentTypeId(1);
+        other.setDateFrom(date);
+        assertFalse(instance.hashCode() == other.hashCode());
+    }
+    
+    @Test
+    public void testObjectsWithNullIdAndOtherIdShallBeTreatedAsDifferent() {
+        System.out.println("testObjectsWithNullIdAndOtherIdShallBeTreatedAsDifferent");
+        Date date = java.util.Calendar.getInstance().getTime();
+        Adjustment instance = new Adjustment();
+        instance.setId(null);
+        instance.setAdjustmentTypeId(1);
+        instance.setDateFrom(date);
+        instance.setDateTo(date);
+        Adjustment other = new Adjustment();
+        other.setId(1);
+        other.setAdjustmentTypeId(1);
+        other.setDateFrom(date);
+        other.setDateTo(date);
+        assertFalse(instance.equals(other));
+    }
+    
+    @Test
+    public void testObjectsWithNullIdAndOtherIdShallHaveDifferentHash() {
+        System.out.println("testObjectsWithNullIdAndOtherIdShallHaveDifferentHash");
+        Date date = java.util.Calendar.getInstance().getTime();
+        Adjustment instance = new Adjustment();
+        instance.setId(null);
+        instance.setAdjustmentTypeId(1);
+        instance.setDateFrom(date);
+        instance.setDateTo(date);
+        Adjustment other = new Adjustment();
+        other.setId(1);
+        other.setAdjustmentTypeId(1);
+        other.setDateFrom(date);
+        other.setDateTo(date);
+        assertFalse(instance.hashCode() == other.hashCode());
+    }
 }
