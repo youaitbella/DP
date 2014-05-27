f625a8070337c5691f0631bbebd6f3d18a4837e7
Stephan Schwarz <stephan.schwarz@inek-drg.de>
2014-05-27 12:41:43 +0000

    Adding more unit tests

--- src/test/java/org/inek/dataportal/entities/modelintention/RemunerationTest.java
+++ src/test/java/org/inek/dataportal/entities/modelintention/RemunerationTest.java
@@ -54,18 +54,146 @@ public class RemunerationTest {
         other.setId(new Long("123456"));
         other.setText("Hallo_2");
         assertTrue(instance.equals(other));
-       // fail();
+    }
+    
+    @Test
+    public void testObjectsWithSameNonNullIdShallHaveSameHash() {
+        System.out.println("testObjectsWithSameNonNullIdShallHaveSameHash");
+        Remuneration instance = new Remuneration();
+        instance.setId(new Long("123456"));
+        instance.setText("Hallo_1");
+        Remuneration other = new Remuneration();
+        other.setId(new Long("123456"));
+        other.setText("Hallo_2");
+        assertEquals(instance.hashCode(), other.hashCode());
     }
     
     @Test
     public void testObjectsWithSameNullIdAndSameTypeAndIndicatorShallBeTreatedAsEqual() {
         System.out.println("testObjectsWithSameNullIdAndSameTypeAndIndicatorShallBeTreatedAsEqual");
         Remuneration instance = new Remuneration();
-        instance.setId(new Long("1"));
-         instance.setText("Equal");
+        instance.setCode("A888999A");
+        instance.setText("Equal");
         Remuneration other = new Remuneration();
-        other.setId(new Long("1"));
+        other.setCode("A888999A");
         other.setText("Equal");
         assertTrue(instance.equals(other));
+    }
+    
+    @Test
+    public void testObjectsWithSameNullIdAndSameTypeAndIndicatorShallHaveSameHash() {
+        System.out.println("testObjectsWithSameNullIdAndSameTypeAndIndicatorShallHaveSameHash");
+        Remuneration instance = new Remuneration();
+        instance.setCode("A888999A");
+        instance.setText("Equal");
+        Remuneration other = new Remuneration();
+        other.setCode("A888999A");
+        other.setText("Equal");
+        assertEquals(instance.hashCode(), other.hashCode());
+    }
+    
+    @Test
+    public void testObjectsWithSameNullIdAndDifferentTypeShallBeTreatedAsDifferent() {
+        System.out.println("testObjectsWithSameNullIdAndDifferentTypeShallBeTreatedAsDifferent");
+        Remuneration instance = new Remuneration();
+        instance.setCode("A888999A");
+        instance.setText("Equal");
+        Remuneration other = new Remuneration();
+        other.setCode("A888999B");
+        other.setText("Equal");
+        assertFalse(instance.equals(other));
+    }
+    
+    @Test
+    public void testObjectsWithSameNullIdAndDifferentTypeShallHaveDifferentHash() {
+        System.out.println("testObjectsWithSameNullIdAndDifferentTypeShallHaveDifferentHash");
+        Remuneration instance = new Remuneration();
+        instance.setCode("A888999A");
+        instance.setText("Equal");
+        Remuneration other = new Remuneration();
+        other.setCode("A888999B");
+        other.setText("Equal");
+        assertFalse(instance.hashCode() == other.hashCode());
+    }
+    
+     @Test
+    public void testObjectsWithSameNullIdAndDifferentIndicatorShallBeTreatedAsDifferent() {
+        System.out.println("testObjectsWithSameNullIdAndDifferentIndicatorShallBeTreatedAsDifferent");
+        Remuneration instance = new Remuneration();
+        instance.setCode("A888999A");
+        instance.setText("Different_1");
+        Remuneration other = new Remuneration();
+        other.setCode("A888999A");
+        other.setText("Different_2");
+        assertFalse(instance.equals(other));
+    }
+    
+    @Test
+    public void testObjectsWithSameNullIdAndDifferentIndicatorShallHaveDifferentHash() {
+        System.out.println("testObjectsWithSameNullIdAndDifferentIndicatorShallHaveDifferentHash");
+        Remuneration instance = new Remuneration();
+        instance.setCode("A888999A");
+        instance.setText("Different_1");
+        Remuneration other = new Remuneration();
+        other.setCode("A888999A");
+        other.setText("Different_2");
+        assertFalse(instance.hashCode() == other.hashCode());
+    }
+    
+    @Test
+    public void testObjectsWithDifferentIdsShallBeTreatedAsDifferent() {
+        System.out.println("testObjectsWithDifferentIdsShallBeTreatedAsDifferent");
+        // abgeändert
+        Remuneration instance = new Remuneration();
+        instance.setId(new Long("123456"));
+        instance.setCode("A888999A");
+        instance.setText("Equal");
+        Remuneration other = new Remuneration();
+        instance.setId(new Long("234567"));
+        other.setCode("A888999A");
+        other.setText("Equal");
+        assertFalse(instance.equals(other));
+    }
+    
+    @Test
+    public void testObjectsWithDifferentIdsShallHaveDifferentHash() {
+        System.out.println("testObjectsWithDifferentIdsShallBeTreatedAsDifferent");
+        Remuneration instance = new Remuneration();
+        instance.setId(new Long("123456"));
+        instance.setCode("A888999A");
+        instance.setText("Equal");
+        Remuneration other = new Remuneration();
+        instance.setId(new Long("234567"));
+        other.setCode("A888999A");
+        other.setText("Equal");
+        assertFalse(instance.hashCode() == other.hashCode());
+    }
+    
+    @Test
+    public void testObjectsWithNullIdAndOtherIdShallBeTreatedAsDifferent() {
+        System.out.println("testObjectsWithNullIdAndOtherIdShallBeTreatedAsDifferent");
+        Remuneration instance = new Remuneration();
+        instance.setId(null);
+        instance.setCode("A888999A");
+        instance.setText("Equal");
+        Remuneration other = new Remuneration();
+        instance.setId(new Long("234567"));
+        other.setCode("A888999A");
+        other.setText("Equal");
+        assertFalse(instance.equals(other));
+    }
+    
+    @Test
+    public void testObjectsWithNullIdAndOtherIdShallHaveDifferentHash() {
+        System.out.println("testObjectsWithNullIdAndOtherIdShallHaveDifferentHash");
+        Remuneration instance = new Remuneration();
+        instance.setId(null);
+        instance.setCode("A888999A");
+        instance.setText("Equal");
+        Remuneration other = new Remuneration();
+        instance.setId(new Long("234567"));
+        other.setCode("A888999A");
+        other.setText("Equal");
+        assertFalse(instance.hashCode() == other.hashCode());
     }
 }
