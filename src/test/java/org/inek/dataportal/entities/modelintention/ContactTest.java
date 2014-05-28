af2f787d1286d8a7308eb3224de6a8f28be441cc
Johannes Schlappa <johannes.schlappa@inek-drg.de>
2014-05-28 15:50:29 +0000

    ContactTest

---
+++ src/test/java/org/inek/dataportal/entities/modelintention/ContactTest.java
@@ -0,0 +1,15 @@
+package org.inek.dataportal.entities.modelintention;
+
+import org.eclipse.persistence.internal.helper.Helper;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author schlappajo
+ */
+public class ContactTest {
+    
+}
