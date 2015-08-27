6fdd93ec72a4aaaf717718b5a866d1f234698aff
Michael Mueller <michael.mueller@inek-drg.de>
2015-08-27 08:04:50 +0000

    enhanced nub handling - test started

---
+++ src/main/java/org/inek/dataportal/helper/faceletvalidators/PredominantPrintableAsciiValidator.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inek.dataportal.helper.faceletvalidators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import org.inek.dataportal.helper.Utils;
+
+/**
+ * Checks whether a required files contains at least one non whitespace
+ * character
+ *
+ * @author muellermi
+ */
+@FacesValidator(value = "PredominantPrintableAsciiValidator")
+public class PredominantPrintableAsciiValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if (value == null) {
+            return;
+        }
+        if (!isValidName("" + value)) {
+            String msg = Utils.getMessage("msgAsciiNeeded");
+            throw new ValidatorException(new FacesMessage(msg));
+        }
+    }
+
+    public boolean isValidName(String name) {
+        String test = name.replaceAll("(\\r|\\n|\\u0085|\\u2028|\\u2029)", ""); // remove line breaks to avoid matching conflicts
+        long asciiCount = test.chars().filter(c -> c >= 32 && c < 127).count();
+        return asciiCount >= test.length() * .9;
+    }
+}
