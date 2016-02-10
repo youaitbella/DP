88e4b429da32912484b75c3376da212eed6958e5
Dominik Vohl <dominik.vohl@inek-drg.de>
2016-02-10 16:56:36 +0000

    begleit-menu

--- Begleitforschung/src/main/java/com/inek/begleitforschung/controller/MainMenu.java
+++ Begleitforschung/src/main/java/com/inek/begleitforschung/controller/MainMenu.java
@@ -6,6 +6,10 @@
 package com.inek.begleitforschung.controller;
 
 import com.inek.begleitforschung.model.MenuModel;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -18,12 +22,8 @@ import org.inek.portallib.tree.MenuTreeNode;
 @Named
 @RequestScoped
 public class MainMenu {
-
-    @Inject private MenuModel _menuModel;
-
-    public MenuTreeNode getMenuRoot() {
-        return _menuModel.getMenuRoot();
-    }
+    
+    private final static String DATA_YEAR_DIR = "W:\\EDV\\Projekte\\InEK-Browsers\\Begleitforschung";
 
     public String callMenu(String url) {
         return url;
@@ -34,5 +34,81 @@ public class MainMenu {
         node.toggle();
         return "";
     }
-
+    
+    public String buildMenu() {
+        String menuHtml = "";
+        for(String dataYear : getDataYearDirs()) {
+            MenuTreeNode root = new MenuModel(Integer.parseInt(dataYear)).getMenuRoot();
+            menuHtml += "<ul class=\"nav \"><li><label label-default=\"\" class=\"tree-toggler nav-header bf-menu-item\">"+root.getText()+"&nbsp;<span class=\"glyphicon glyphicon-menu-down\" aria-hidden=\"true\"></span></label>";
+            menuHtml += "<ul class=\"nav tree active-trial\">";
+            for(MenuTreeNode node1 : root.getChildren()) {
+                if(node1.getChildren().isEmpty()) {
+                    menuHtml += "<li><a class=\"bf-menu-item bf-menu-item-2\" href=\""+node1.getUrl()+"\">"+node1.getText()+"</a>";
+                } else {
+                    menuHtml += "<li><label label-default=\"\" class=\"tree-toggler nav-header bf-menu-item bf-menu-item-2\">"+node1.getText()+"&nbsp;<span class=\"glyphicon glyphicon-menu-down\" aria-hidden=\"true\"></span></label>";
+                    menuHtml += "<ul class=\"nav tree active-trial\">";
+                    for(MenuTreeNode node2 : node1.getChildren()) {
+                        if(node2.getChildren().isEmpty()) {
+                            menuHtml += "<li><a class=\"bf-menu-item bf-menu-item-3\" href=\""+node2.getUrl()+"\">"+node2.getText()+"</a>";
+                        } else {
+                            menuHtml += "<li><label label-default=\"\" class=\"tree-toggler nav-header bf-menu-item bf-menu-item-3\">"+node2.getText()+"&nbsp;<span class=\"glyphicon glyphicon-menu-down\" aria-hidden=\"true\"></span></label>";
+                            menuHtml += "<ul class=\"nav tree active-trial\">";
+                            for(MenuTreeNode node3 : node2.getChildren()) {
+                                if(node3.getChildren().isEmpty()) {
+                                    menuHtml += "<li><a class=\"bf-menu-item bf-menu-item-4\" href=\""+node3.getUrl()+"\">"+node3.getText()+"</a>";
+                                } else {
+                                    menuHtml += "<li><label label-default=\"\" class=\"tree-toggler nav-header bf-menu-item bf-menu-item-4\">"+node3.getText()+"&nbsp;<span class=\"glyphicon glyphicon-menu-down\" aria-hidden=\"true\"></span></label>";
+                                    menuHtml += "<ul class=\"nav tree active-trial\">";
+                                    for(MenuTreeNode node4 : node3.getChildren()) {
+                                        if(node4.getChildren().isEmpty()) {
+                                            menuHtml += "<li><a class=\"bf-menu-item bf-menu-item-5\" href=\""+node4.getUrl()+"\">"+node4.getText()+"</a>";
+                                        } else {
+                                            menuHtml += "<li><label label-default=\"\" class=\"tree-toggler nav-header bf-menu-item bf-menu-item-5\">"+node4.getText()+"&nbsp;<span class=\"glyphicon glyphicon-menu-down\" aria-hidden=\"true\"></span></label>";
+                                            menuHtml += "<ul class=\"nav tree active-trial\">";
+                                            for(MenuTreeNode node5 : node4.getChildren()) {
+                                                if(node5.getChildren().isEmpty()) {
+                                                    menuHtml += "<li><a class=\"bf-menu-item bf-menu-item-6\" href=\""+node5.getUrl()+"\">"+node5.getText()+"</a>";
+                                                } else{
+                                                    menuHtml += "<li><label label-default=\"\" class=\"tree-toggler nav-header bf-menu-item bf-menu-item-6\">"+node5.getText()+"&nbsp;<span class=\"glyphicon glyphicon-menu-down\" aria-hidden=\"true\"></span></label>";
+                                                    menuHtml += "<ul class=\"nav tree active-trial\">";
+                                                    menuHtml += "</ul>";
+                                                    menuHtml += "</li>";
+                                                }
+                                            }
+                                            menuHtml += "</ul>";
+                                            menuHtml += "</li>";
+                                        }
+                                    }
+                                    menuHtml += "</ul>";
+                                    menuHtml += "</li>";
+                                }
+                            }
+                            menuHtml += "</ul>";
+                            menuHtml += "</li>";
+                        }
+                    }
+                    menuHtml += "</ul>";
+                    menuHtml += "</li>";
+                }
+            }
+            menuHtml += "</ul>";
+            menuHtml += "</li>";
+            menuHtml += "</ul>";
+        }
+        return menuHtml;
+    }
+    
+    private List<String> getDataYearDirs() {
+        String[] dirs = new File(DATA_YEAR_DIR).list();
+        List<String> dataYears = new ArrayList<>();
+        for(String dir : dirs) {
+            try {
+                Integer.parseInt(dir);
+                dataYears.add(dir);
+            } catch(Exception ex) {
+                continue;
+            }
+        }
+        return dataYears;
+    }
 }
--- Begleitforschung/src/main/java/com/inek/begleitforschung/model/MenuModel.java
+++ Begleitforschung/src/main/java/com/inek/begleitforschung/model/MenuModel.java
@@ -6,47 +6,46 @@
 package com.inek.begleitforschung.model;
 
 import java.io.Serializable;
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
-import javax.inject.Named;
 import org.inek.portallib.tree.MenuTreeNode;
 
 /**
  *
  * @author muellermi
  */
-@SessionScoped
-@Named
 public class MenuModel implements Serializable{
     private MenuTreeNode _menuRoot;
-
-    public MenuTreeNode getMenuRoot() {
-        return _menuRoot;
-    }
+    private int _dataYear;
     
-    @PostConstruct
-    private void Init(){
-        _menuRoot = MenuTreeNode.createRoot("", "");
-        MenuTreeNode node1 = _menuRoot.addChild("Datenbasis", "/views/Participation.xhtml");
-        node1.addChild("Beteiligung an der Datenübermittlung", "/views/Participation1.xhtml");
-        node1.addChild("Datenqualität", "/views/Participation2.xhtml");
+    public MenuModel(int year) {
+        _dataYear = year;
+        _menuRoot = MenuTreeNode.createRoot(_dataYear+"", "");
+        MenuTreeNode node1 = _menuRoot.addChild("Datenbasis", "/views/Participation.xhtml?dataYear=" + _dataYear);
+        node1.addChild("Beteiligung an der Datenübermittlung", "/views/Participation1.xhtm?dataYear=" + _dataYear);
+        node1.addChild("Datenqualität", "/views/Participation2.xhtml?dataYear="+_dataYear);
         node1.addChild("Unspezifische Kodierung", "/views/Participation3.xhtml");
         
         node1 = _menuRoot.addChild("KH-Strukturdaten", "/views/Participation4.xhtml");
         node1.addChild("Größenklassen (Bettenzahl) / Bundesland", "/views/Participation5.xhtml");
         
         node1 = _menuRoot.addChild("G-DRG-System", "/views/Participation6.xhtml");
-        MenuTreeNode node2 = node1.addChild("Hauptabteilungen", "/views/Participation7.xhtml");
+        MenuTreeNode node2 = node1.addChild("Hauptabteilungen", "");
         node2.addChild("wenig komplexe Leistungen", "/views/Participation8.xhtml");
         node2.addChild("komplexe Leistungen", "/views/Participation9.xhtml");
         node2.addChild("häufige Leistungen", "/views/Participationa.xhtml");
 
         node1 = _menuRoot.addChild("Vollstationäre Falldaten", "/views/Participationb.xhtml");
         node2 = node1.addChild("Versorgung in den Hauptabteilungen", "/views/Participationc.xhtml");
-        MenuTreeNode node3 = node2.addChild("Demographische und medizinische Merkmale", "/views/Participationd.xhtml");
-        MenuTreeNode node4 = node3.addChild("nach Hauptdiagnosen", "/views/Participatione.xhtml");
+        MenuTreeNode node3 = node2.addChild("Demographische und medizinische Merkmale", "");
+        MenuTreeNode node4 = node3.addChild("nach Hauptdiagnosen", "");
         node4.addChild("HD Kapitel", "/views/Participationf.xhtml");
         node4.addChild("HD Gruppe", "/views/Participationg.xhtml");
     }
-   
+    
+    public MenuTreeNode getMenuRoot() {
+        return _menuRoot;
+    }
+
+    public int getDataYear() {
+        return _dataYear;
+    }
 }
