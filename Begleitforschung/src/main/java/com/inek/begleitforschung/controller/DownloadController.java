b8d0e1fb98a7dfedd4ac8db4577a680b91168e37
Michael Mueller <michael.mueller@inek-drg.de>
2016-07-27 08:32:45 +0000

    navigation changed nocachefilter added as a precaution

--- Begleitforschung/src/main/java/com/inek/begleitforschung/controller/DownloadController.java
+++ Begleitforschung/src/main/java/com/inek/begleitforschung/controller/DownloadController.java
@@ -11,14 +11,17 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import javax.enterprise.context.ApplicationScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 
 /**
  *
  * @author vohldo
  */
-
+@Named
+@ApplicationScoped
 public class DownloadController {
     
     
