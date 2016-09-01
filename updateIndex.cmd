dfcd3c7cc89122874d34e4339b07ba2d16de3e65
Michael Müller <michael.mueller@inek-drg.de>
2016-09-01 14:22:45 +0200

    updateIndex batch file

---
+++ updateIndex.cmd
@@ -0,0 +1,3 @@
+git update-index --assume-unchanged DataPortal\src\main\webapp\resources\manual\InEK-Datenportal.pdf
+git update-index --assume-unchanged DataPortal\src\main\webapp\resources\manual\InEK-DatenportalIntern.pdf
+git update-index --assume-unchanged DataPortal\src\main\webapp\resources\manual\InEK-DatenportalZerti.pdf
