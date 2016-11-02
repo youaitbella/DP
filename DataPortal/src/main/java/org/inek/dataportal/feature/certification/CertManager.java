90bcacd524e56d934521f27b7e1ec579f8073ba8
vohldo <dominik.vohl@inek-drg.de>
2016-11-02 09:43:15 +0100

    NUB message for document tab, if not saved.

--- DataPortal/src/main/java/org/inek/dataportal/feature/certification/CertManager.java
+++ DataPortal/src/main/java/org/inek/dataportal/feature/certification/CertManager.java
@@ -5,7 +5,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
