3a27a4ad4322b509a63309698e42e0c47fd0cb81
Michael Müller <michael.mueller@inek-drg.de>
2016-12-19 13:09:53 +0100

    reneame entity

--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/CalcHeaderText.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/CalcHeaderText.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "KGLListHeaderText", schema = "calc")
-public class CalcBasicsHeadline implements Serializable {
+public class CalcHeaderText implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
