cff974bbfeba01792b82d3b95449e9d1929267d0
Andreas Kunkel <andreas.kunkel@inek-drg.de>
2017-01-02 11:24:43 +0100

    fix JPA Queries for DRGSheetName to member fields

--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/DRGSheetName.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/DRGSheetName.java
@@ -31,9 +31,9 @@ import javax.xml.bind.annotation.XmlTransient;
 @Table(name = "KGLListSheet", schema = "calc")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "KGLListSheet.findAll", query = "SELECT k FROM KGLListSheet k")
-    , @NamedQuery(name = "KGLListSheet.findBySID", query = "SELECT k FROM KGLListSheet k WHERE k.sID = :sID")
-    , @NamedQuery(name = "KGLListSheet.findBySSheet", query = "SELECT k FROM KGLListSheet k WHERE k.sSheet = :sSheet")})
+    @NamedQuery(name = "DRGSheetName.findAll", query = "SELECT k FROM DRGSheetName k")
+    , @NamedQuery(name = "DRGSheetName.findBySID", query = "SELECT k FROM DRGSheetName k WHERE k._id = :sID")
+    , @NamedQuery(name = "DRGSheetName.findBySSheet", query = "SELECT k FROM DRGSheetName k WHERE k._sheet = :sSheet")})
 public class DRGSheetName implements Serializable {
 
     private static final long serialVersionUID = 1L;
