0c39f120304b5e8654844f3c5025911c49001758
Andreas Kunkel <andreas.kunkel@inek-drg.de>
2017-01-05 15:41:26 +0100

    remove unused imports

--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLListMedInfra.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLListMedInfra.java
@@ -6,14 +6,9 @@
 package org.inek.dataportal.entities.calc;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
