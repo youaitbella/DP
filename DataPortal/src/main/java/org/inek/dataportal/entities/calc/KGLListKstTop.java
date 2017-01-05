8464a0afbac79d388fab1e962f79992578869378
Andreas Kunkel <andreas.kunkel@inek-drg.de>
2017-01-05 12:07:41 +0100

    rename member variables

--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLListKstTop.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLListKstTop.java
@@ -6,7 +6,6 @@
 package org.inek.dataportal.entities.calc;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,118 +25,133 @@ public class KGLListKstTop implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    //<editor-fold defaultstate="collapsed" desc="Property id">
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ktID")
-    private int id = -1;
-
+    private int _id = -1;
+    
     public int getId() {
-        return id;
+        return _id;
     }
-
+    
     public void setId(int id) {
-        this.id = id;
+        this._id = id;
     }
-
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Property baseInformationID">
     @Column(name = "ktBaseInformationID")
-    private int baseInformationID;
+    private int _baseInformationID;
 
     public int getBaseInformationID() {
-        return baseInformationID;
+        return _baseInformationID;
     }
 
     public void setBaseInformationID(int baseInformationID) {
-        this.baseInformationID = baseInformationID;
+        this._baseInformationID = baseInformationID;
     }
+    //</editor-fold>
 
     
+    //<editor-fold defaultstate="collapsed" desc="Property ktCostCenterID">
     @Column(name = "ktCostCenterID")
-    private int ktCostCenterID;
+    private int _ktCostCenterID;
 
     public int getKtCostCenterID() {
-        return ktCostCenterID;
+        return _ktCostCenterID;
     }
 
     public void setKtCostCenterID(int ktCostCenterID) {
-        this.ktCostCenterID = ktCostCenterID;
+        this._ktCostCenterID = ktCostCenterID;
     }
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="Property text">
     @Column(name = "ktText")
-    private String text = "";
+    private String _text = "";
 
     public String getText() {
-        return text;
+        return _text;
     }
 
     public void setText(String text) {
-        this.text = text;
+        this._text = text;
     }
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="Property caseCount">
     @Column(name = "ktCaseCnt")
-    private int caseCount;
+    private int _caseCount;
 
     public int getCaseCount() {
-        return caseCount;
+        return _caseCount;
     }
 
     public void setCaseCount(int caseCount) {
-        this.caseCount = caseCount;
+        this._caseCount = caseCount;
     }
+    //</editor-fold>
 
-    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    //<editor-fold defaultstate="collapsed" desc="Property amount">
     @Column(name = "ktAmount")
-    private int amount;
+    private int _amount;
 
     public int getAmount() {
-        return amount;
+        return _amount;
     }
 
     public void setAmount(int amount) {
-        this.amount = amount;
+        this._amount = amount;
     }
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="Property delimitationAmount">
     @Column(name = "ktDelimitationAmount")
-    private int delimitationAmount;
+    private int _delimitationAmount;
 
     public int getDelimitationAmount() {
-        return delimitationAmount;
+        return _delimitationAmount;
     }
 
     public void setDelimitationAmount(int delimitationAmount) {
-        this.delimitationAmount = delimitationAmount;
+        this._delimitationAmount = delimitationAmount;
     }
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="Property ">
     @Column(name = "ktRank")
-    private int rank;
+    private int _rank;
 
     public int getRank() {
-        return rank;
+        return _rank;
     }
 
     public void setRank(int rank) {
-        this.rank = rank;
+        this._rank = rank;
     }
+    //</editor-fold>
 
     public KGLListKstTop() {
     }
 
     public KGLListKstTop(Integer ktID) {
-        this.id = ktID;
+        this._id = ktID;
     }
 
     public KGLListKstTop(Integer ktID, int ktCostCenterID, String ktText, int ktCaseCnt, int ktAmount, int ktDelimitationAmount, int ktRank) {
-        this.id = ktID;
-        this.ktCostCenterID = ktCostCenterID;
-        this.text = ktText;
-        this.caseCount = ktCaseCnt;
-        this.amount = ktAmount;
-        this.delimitationAmount = ktDelimitationAmount;
-        this.rank = ktRank;
+        this._id = ktID;
+        this._ktCostCenterID = ktCostCenterID;
+        this._text = ktText;
+        this._caseCount = ktCaseCnt;
+        this._amount = ktAmount;
+        this._delimitationAmount = ktDelimitationAmount;
+        this._rank = ktRank;
     }
 
     @Override
     public int hashCode() {
-        return id;
+        return _id;
     }
 
     @Override
@@ -147,12 +161,12 @@ public class KGLListKstTop implements Serializable {
             return false;
         }
         KGLListKstTop other = (KGLListKstTop) object;
-        return this.id == other.id;
+        return this._id == other._id;
     }
 
     @Override
     public String toString() {
-        return "org.inek.dataportal.entities.calc.KGLListKstTop[ ktID=" + id + " ]";
+        return "org.inek.dataportal.entities.calc.KGLListKstTop[ ktID=" + _id + " ]";
     }
 
 }
