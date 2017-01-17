51fcc7c134eb67be7f83f0f6aa65f0356d3d098c
vohldo <dominik.vohl@inek-drg.de>
2017-01-17 17:03:15 +0100

    normalward, wip

--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLNormalFreelancer.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLNormalFreelancer.java
@@ -11,7 +11,9 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -136,6 +138,7 @@ public class KGLNormalFreelancer implements Serializable {
     }
     //</editor-fold>
     
+    //</editor-fold>
 
     public KGLNormalFreelancer() {
     }
