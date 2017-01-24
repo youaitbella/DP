258eee5f7157ae1d6d4257efeb23eda89b74aea3
Andreas Kunkel <andreas.kunkel@inek-drg.de>
2017-01-24 17:10:54 +0100

    add properties due to database change KGPDocuments

--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGPDocuments.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGPDocuments.java
@@ -6,9 +6,12 @@
 package org.inek.dataportal.entities.calc;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
@@ -25,111 +28,140 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author kunkelan
  */
 @Entity
-@Table(name = "KGPDocuments", catalog = "DataPortalDev", schema = "calc")
+@Table(name = "KGPDocuments", schema = "calc")
 @XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "KGPDocuments.findAll", query = "SELECT k FROM KGPDocuments k")})
 public class KGPDocuments implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    // <editor-fold defaultstate="collapsed" desc="Property Id">
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @NotNull
     @Column(name = "doID")
-    private Integer doID;
+    private int _id = -1;
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int id) {
+        this._id = id;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Property _name">
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 250)
+    @Size( max = 250)
     @Column(name = "doName")
-    private String doName;
+    private String _name = "";
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        this._name = name;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Property _content">
     @Basic(optional = false)
     @NotNull
     @Lob
     @Column(name = "doContent")
-    private byte[] doContent;
-    @JoinColumn(name = "doBaseInformationID", referencedColumnName = "biID")
-    @ManyToOne(optional = false)
-    private PeppCalcBasics doBaseInformationID;
-    @JoinColumn(name = "doSheetID", referencedColumnName = "sID")
-    @ManyToOne(optional = false)
-    private KGPListSheet doSheetID;
+    private byte[] _content = new byte[0];
+
+    public byte[] getContent() {
+        return _content;
+    }
+
+    public void setContent(byte[] content) {
+        this._content = content;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Property _aseInformationId">
+//    @JoinColumn(name = "doBaseInformationID", referencedColumnName = "biID")
+//    @ManyToOne(optional = false)
+    @Column(name = "doBaseInformationID")
+    //private PeppCalcBasics _aseInformationId;
+    private int _aseInformationId;
+
+    public int getAseInformationId() {
+        return _aseInformationId;
+    }
+
+    public void setAseInformationId(int aseInformationId) {
+        this._aseInformationId = aseInformationId;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Property _sheetId">
+//    @JoinColumn(name = "doSheetID", referencedColumnName = "sID")
+//    @ManyToOne(optional = false)
+    @Column(name = "doSheetID")
+    //private KGPListSheet _sheetId;
+    private int _sheetId;
+
+    public int getSheetId() {
+        return _sheetId;
+    }
+
+    public void setSheetId(int sheetId) {
+        this._sheetId = sheetId;
+    }
+    // </editor-fold>
 
     public KGPDocuments() {
     }
 
-    public KGPDocuments(Integer doID) {
-        this.doID = doID;
-    }
-
-    public KGPDocuments(Integer doID, String doName, byte[] doContent) {
-        this.doID = doID;
-        this.doName = doName;
-        this.doContent = doContent;
-    }
-
-    public Integer getDoID() {
-        return doID;
-    }
-
-    public void setDoID(Integer doID) {
-        this.doID = doID;
-    }
-
-    public String getDoName() {
-        return doName;
-    }
-
-    public void setDoName(String doName) {
-        this.doName = doName;
-    }
-
-    public byte[] getDoContent() {
-        return doContent;
-    }
-
-    public void setDoContent(byte[] doContent) {
-        this.doContent = doContent;
-    }
-
-    public PeppCalcBasics getDoBaseInformationID() {
-        return doBaseInformationID;
-    }
-
-    public void setDoBaseInformationID(PeppCalcBasics doBaseInformationID) {
-        this.doBaseInformationID = doBaseInformationID;
-    }
-
-    public KGPListSheet getDoSheetID() {
-        return doSheetID;
-    }
-
-    public void setDoSheetID(KGPListSheet doSheetID) {
-        this.doSheetID = doSheetID;
-    }
-
+//<editor-fold defaultstate="collapsed" desc="hash && equals && toString">
     @Override
     public int hashCode() {
-        int hash = 0;
-        hash += (doID != null ? doID.hashCode() : 0);
+        int hash = 3;
+        hash = 73 * hash + this._id;
+        if (_id != -1) return hash;
+        hash = 73 * hash + Objects.hashCode(this._name);
+        hash = 73 * hash + this._aseInformationId;
+        hash = 73 * hash + this._sheetId;
         return hash;
     }
-
+    
     @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof KGPDocuments)) {
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof KGPDocuments)) {
             return false;
         }
-        KGPDocuments other = (KGPDocuments) object;
-        if ((this.doID == null && other.doID != null) || (this.doID != null && !this.doID.equals(other.doID))) {
+        final KGPDocuments other = (KGPDocuments) obj;
+        if (this._id != -1 && this._id == other._id) {
+            return true;
+        }
+        if (this._id != other._id) {
+            return false;
+        }
+        if (this._aseInformationId != other._aseInformationId) {
+            return false;
+        }
+        if (this._sheetId != other._sheetId) {
+            return false;
+        }
+        if (!Objects.equals(this._name, other._name)) {
             return false;
         }
         return true;
     }
-
+    
+    
     @Override
     public String toString() {
-        return "org.inek.dataportal.entities.calc.KGPDocuments[ doID=" + doID + " ]";
+        return "org.inek.dataportal.entities.calc.KGPDocuments[ _ID=" + _id + " ]";
     }
+//</editor-fold>
     
 }
