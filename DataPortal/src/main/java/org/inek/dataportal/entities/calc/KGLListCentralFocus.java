2db803ea9dee29c23ffb32e795322646140b0048
vohldo <dominik.vohl@inek-drg.de>
2017-02-21 17:18:17 +0100

    strategy auto generation

--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLListCentralFocus.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLListCentralFocus.java
@@ -7,14 +7,12 @@ package org.inek.dataportal.entities.calc;
 
 import java.io.Serializable;
 import java.util.Objects;
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
--- DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLPKMSAlternative.java
+++ DataPortal/src/main/java/org/inek/dataportal/entities/calc/KGLPKMSAlternative.java
@@ -10,6 +10,8 @@ import java.util.Objects;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -29,8 +31,7 @@ public class KGLPKMSAlternative implements Serializable {
     
     //<editor-fold defaultstate="collapsed" desc="Property _id">
     @Id
-    @Basic(optional = false)
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "paID")
     private int _id = -1;
 
