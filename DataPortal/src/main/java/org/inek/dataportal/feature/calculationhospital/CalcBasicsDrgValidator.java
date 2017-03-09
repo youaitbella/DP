964581369e90093eefd60708f0143c5f54c262bb
Michael Müller <michael.mueller@inek-drg.de>
2017-03-09 10:00:51 +0100

    global validation prepared

--- DataPortal/src/main/java/org/inek/dataportal/feature/calculationhospital/CalcBasicsDrgValidator.java
+++ DataPortal/src/main/java/org/inek/dataportal/feature/calculationhospital/CalcBasicsDrgValidator.java
@@ -18,11 +18,119 @@ public class CalcBasicsDrgValidator {
    public static MessageContainer composeMissingFieldsMessage(DrgCalcBasics calcBasics) {
         MessageContainer message = new MessageContainer();
 
-        checkField(message, calcBasics.getIk(), 100000000, 999999999, "lblIK", "form:ikMulti", "todo:page");
+        checkBasics(calcBasics, message);
+        checkBasicExplanation(calcBasics, message);
+        checkExternalServiceProvision(calcBasics, message);
+        checkOpAn(calcBasics, message);
+        checkMaternityRoom(calcBasics, message);
+        checkCardiology(calcBasics, message);
+        checkEndosoppy(calcBasics, message);
+        checkRadiology(calcBasics, message);
+        checkLaboratory(calcBasics, message);
+        checkDiagnosticScope(calcBasics, message);
+        checkNormalWard(calcBasics, message);
+        checkIntensiveCare(calcBasics, message);
+        checkStrokeUnit(calcBasics, message);
+        checkMedicalInfrastructure(calcBasics, message);
+        checkNonMedicalInfrastructure(calcBasics, message);
+        checkStaffCost(calcBasics, message);
+        checkValvularIntervention(calcBasics, message);
+        checkNeonat(calcBasics, message);
 
         return message;
     }
 
+    //<editor-fold defaultstate="collapsed" desc="checkBasics">
+   private static void checkBasics(DrgCalcBasics calcBasics, MessageContainer message) {
+       checkField(message, calcBasics.getIk(), 100000000, 999999999, "lblIK", "form:ikMulti", "todo:page");
+   }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkBasicExplanation">
+    private static void checkBasicExplanation(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkExternalServiceProvision">
+    private static void checkExternalServiceProvision(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkOpAn">
+    private static void checkOpAn(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkMaternityRoom">
+    private static void checkMaternityRoom(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkCardiology">
+    private static void checkCardiology(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkEndosoppy">
+    private static void checkEndosoppy(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkRadiology">
+    private static void checkRadiology(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkLaboratory">
+    private static void checkLaboratory(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkDiagnosticScope">
+    private static void checkDiagnosticScope(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkNormalWard">
+    private static void checkNormalWard(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkIntensiveCare">
+    private static void checkIntensiveCare(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkStrokeUnit">
+    private static void checkStrokeUnit(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkMedicalInfrastructure">
+    private static void checkMedicalInfrastructure(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkNonMedicalInfrastructure">
+    private static void checkNonMedicalInfrastructure(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkStaffCost">
+    private static void checkStaffCost(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkValvularIntervention">
+    private static void checkValvularIntervention(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="checkNeonat">
+    private static void checkNeonat(DrgCalcBasics calcBasics, MessageContainer message) {
+    }
+    //</editor-fold>
+    
     private static void checkField(MessageContainer message, String value, String msgKey, String elementId, String topicName) {
         if (Utils.isNullOrEmpty(value)) {
             applyMessageValues(message, msgKey, topicName, elementId);
@@ -44,5 +152,5 @@ public class CalcBasicsDrgValidator {
             message.setElementId(elementId);
         }
     }
-    
+
 }
