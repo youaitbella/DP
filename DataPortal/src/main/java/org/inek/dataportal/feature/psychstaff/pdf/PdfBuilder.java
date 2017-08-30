0523b914e78eadbf9b4a1c7215d257baa041175a
Youness Ait Bella <youness.aitbella@inek-drg.de>
2017-08-30 16:45:22 +0200

    PDFBuilder wip

--- DataPortal/src/main/java/org/inek/dataportal/feature/psychstaff/pdf/PdfBuilder.java
+++ DataPortal/src/main/java/org/inek/dataportal/feature/psychstaff/pdf/PdfBuilder.java
@@ -10,12 +10,9 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -23,12 +20,10 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.net.MalformedURLException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
 import javax.inject.Inject;
-import javax.xml.bind.DatatypeConverter;
 import org.inek.dataportal.feature.psychstaff.backingbean.EditPsyStaff;
 import org.inek.dataportal.feature.psychstaff.entity.OccupationalCatagory;
 import org.inek.dataportal.feature.psychstaff.enums.PsychType;
@@ -52,8 +47,6 @@ public class PdfBuilder implements Serializable {
     private static final Font SMALL = new Font(Font.getFamily("TIMES_ROMAN"), 6, Font.NORMAL);
     private static final Font SSMALL = new Font(Font.FontFamily.HELVETICA, 6);
     //</editor-fold>
-
-    private final String fout = "D:\\projects\\DataPortal\\DataPortal\\PsychStaffDocument.pdf";
     private final String img = "D:\\projects\\DataPortal\\DataPortal\\InEK.gif";
 
     //<editor-fold defaultstate="collapsed" desc="PdfBuilder">
@@ -64,37 +57,35 @@ public class PdfBuilder implements Serializable {
     //<editor-fold defaultstate="collapsed" desc="createDocument">
     public void createDocument() throws DocumentException, FileNotFoundException, IOException,
             BadElementException, MalformedURLException, NoSuchAlgorithmException {
-        String fileout = fout;
+
+        String fileout = "D:\\projects\\DataPortal\\DataPortal\\"+_editPsyStaff.getActiveTopic().getTitle().trim()+".pdf";
         Document document = new Document();
         PdfWriter.getInstance(document, new FileOutputStream(fileout));
         document.open();
-        Paragraph p = new Paragraph();
-        Chunk c;
 
         createMetadaten(document);
         addLogo(document, img);
-        addTitle(document, p);
-        createTableInfoandChecksum(document, fout);
+        addTitle(document, _editPsyStaff.getActiveTopic().getTitle());
+        createTableInfoandChecksum(document, _editPsyStaff.getActiveTopic().getTitle());
 
         //<editor-fold defaultstate="collapsed" desc="Table PsyStaff">
-        PdfPTable tb2 = new PdfPTable(6);
-        tb2.setWidths(new int[]{4, 1, 3, 3, 3, 3});
-        tb2.getDefaultCell().setBackgroundColor(BaseColor.LIGHT_GRAY);
+        PdfPTable tb = new PdfPTable(6);
+        tb.setWidths(new int[]{3, 1, 3, 3, 3, 3});
+        tb.getDefaultCell().setBackgroundColor(BaseColor.LIGHT_GRAY);
 
         List<String> headers = Arrays.asList("\nPersonalgruppen", "\nLfd. Nr.",
                 "\nBerufsgruppen der Psych-PV",
                 "Stellenbesetzung für \neine vollständige Umsetzung der Psych-\nPV in VK",
                 "Stellenbesetzung \nals \nBudgetgrundlage in VK",
-                "Durschnittskosten \nin EURO");
-        addHeader(tb2, headers);
+                "\nDurschnittskosten \nin EURO");
+        addHeader(tb, headers);
         headers = Arrays.asList("", "", "1", "2", "3", "4");
-        addHeader(tb2, headers);
+        addHeader(tb, headers);
 
         int index = 1;
-        double gesamtStaffingComplete = 0.0, gesamtStaffingBudget = 0.0, gesamtAvgCost = 0.0;
+        double sumStaffingComplete = 0.0, sumStaffingBudget = 0.0, AvgCosts = 0.0;
         for (OccupationalCatagory occupationalCatagory : _editPsyStaff.getOccupationalCategories()) {
-            System.out.println("" + occupationalCatagory.getName() + "-");
-            addRow(tb2, occupationalCatagory.getPersonnelGroup().getName(),
+            addRow(tb, occupationalCatagory.getPersonnelGroup().getName(),
                     String.valueOf(index),
                     occupationalCatagory.getName(),
                     _editPsyStaff
@@ -115,22 +106,40 @@ public class PdfBuilder implements Serializable {
                                     .getId())
                             .getAvgCost());
             index++;
+            sumStaffingComplete += _editPsyStaff
+                    .getStaffProof()
+                    .getStaffProofsAgreed(PsychType.Adults)
+                    .get(occupationalCatagory.getPersonnelGroup()
+                            .getId())
+                    .getStaffingComplete();
+            sumStaffingBudget += _editPsyStaff.getStaffProof()
+                    .getStaffProofsAgreed(PsychType.Adults)
+                    .get(occupationalCatagory.getPersonnelGroup()
+                            .getId())
+                    .getStaffingBudget();
+            AvgCosts += _editPsyStaff
+                            .getStaffProof()
+                            .getStaffProofsAgreed(PsychType.Adults)
+                            .get(occupationalCatagory.getPersonnelGroup()
+                                    .getId())
+                            .getAvgCost();
         }
-        headers = Arrays.asList("", "8", "Gesamt", String.valueOf(gesamtStaffingComplete),
-                String.valueOf(gesamtStaffingBudget),
-                String.valueOf(gesamtAvgCost));
-        addHeader(tb2, headers);
+        AvgCosts /= _editPsyStaff.getOccupationalCategories().size();
+        headers = Arrays.asList("", "8", "Gesamt", String.valueOf(sumStaffingComplete),
+                String.valueOf(sumStaffingBudget),
+                String.valueOf(AvgCosts));
+        addHeader(tb, headers);
 
-        tb2.setSpacingAfter(20);
-        document.add(tb2);
+        tb.setSpacingAfter(20);
+        document.add(tb);
         //</editor-fold>
 
         addInfoText(document, "1. Die vereinbarten Berechnungstage in Anlage 1 und die tatsächlichen "
-                + "Berechnungstage in Anlage 2 sind in einer einheitlichen Zählweise entweder nach LKA oder nach PEPPV anzugeben.", 0);
+                + "Berechnungstage in Anlage 2 sind in einer einheitlichen Zählweise entweder nach LKA \noder nach PEPPV anzugeben.", 0);
         addInfoText(document, "2. Bei Kinder- und Jugendpsychiatrie einschließlich Erziehungsdienst", 20);
         addInfoText(document, "Diese Datei ist durch die Vertragspartner nach $11 BPflV zu unterschreiben "
                 + "und als elektronische Kopie an das InEK zu senden", 50);
-        addSignatureArea(document);
+        addSignaturArea(document);
         addFooter(document);
         document.close();
     }
@@ -138,13 +147,13 @@ public class PdfBuilder implements Serializable {
 
     //<editor-fold defaultstate="collapsed" desc="createMetadaten">
     private void createMetadaten(Document document) {
-        document.addTitle("Title");
-        document.addSubject("Subject");
-        document.addKeywords("ort" + ", " + "nachName");
-        document.addAuthor("Author: " + "getVorName()" + " " + "getNachName()");
-        document.addCreator("nachName");
+        document.addTitle("");
+        document.addSubject("");
+        document.addKeywords("" + ", " + "");
+        document.addAuthor("" + "" + " " + "");
+        document.addCreator("");
         document.addCreationDate();
-        document.addHeader("ort", "vorName");
+        document.addHeader("", "");
         document.addProducer();
     }
     //</editor-fold>    
@@ -169,17 +178,17 @@ public class PdfBuilder implements Serializable {
     //</editor-fold>    
 
     //<editor-fold defaultstate="collapsed" desc="addTitle">
-    private void addTitle(Document document, Paragraph p) throws DocumentException {
-        p = new Paragraph("Export: Anlage 1", FONT_TITLE);
+    private void addTitle(Document document, String s) throws DocumentException {
+        Paragraph p = new Paragraph("Export: " + s , FONT_TITLE);
         p.setSpacingAfter(50);
-        p.setIndentationLeft(30);// Abstand von links
+        p.setIndentationLeft(30);
         document.add(p);
     }
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="addHeader">
-    private void addHeader(PdfPTable tb, List l) {
-        l.stream().forEach(e -> addCell(tb, e.toString(), SMALLBOLD, Element.ALIGN_CENTER, BaseColor.LIGHT_GRAY));
+    private void addHeader(PdfPTable tb, List <String> l) {
+        l.stream().forEach(e -> addCell(tb, e, SMALLBOLD, Element.ALIGN_CENTER, BaseColor.LIGHT_GRAY));
     }
     //</editor-fold>    
 
@@ -195,32 +204,7 @@ public class PdfBuilder implements Serializable {
         addCell(tb2, String.valueOf(staffingBudget), SMALL, Element.ALIGN_LEFT, BaseColor.WHITE);
         addCell(tb2, String.valueOf(avgCost), SMALL, Element.ALIGN_LEFT, BaseColor.WHITE);
     }
-    //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="addRowWithSpan">
-    private void addRowWithSpan(PdfPTable tb, String pg) {
-        PdfPCell cell;
-        // 3. row
-        cell = new PdfPCell(new Paragraph(pg, SMALL));
-        cell.setRowspan(4);
-        cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
-        tb.addCell(cell);
-        addSpanedRow(tb, "3", "Psychologen", "", "", "");
-        addSpanedRow(tb, "4", "Sozialarbeiter", "", "", "");
-        addSpanedRow(tb, "5", "Bewegungstherapeuten", "", "", "");
-        addSpanedRow(tb, "6", "Logopäden (nur KJP)", "", "", "");
-    }
-    //</editor-fold>    
-
-    //<editor-fold defaultstate="collapsed" desc="addSpanedRow">
-    private void addSpanedRow(PdfPTable tb, String lfdNr, String bg, String stellenbesetzungPVinVK, String stellenbesetzungVK, String sum) {
-        addCell(tb, lfdNr, SMALL, Element.ALIGN_CENTER, BaseColor.LIGHT_GRAY);
-        addCell(tb, bg, SMALL, Element.ALIGN_LEFT, BaseColor.LIGHT_GRAY);
-        addCell(tb, stellenbesetzungPVinVK, SMALL, Element.ALIGN_CENTER, BaseColor.WHITE);
-        addCell(tb, stellenbesetzungVK, SMALL, Element.ALIGN_CENTER, BaseColor.WHITE);
-        addCell(tb, sum, SMALL, Element.ALIGN_CENTER, BaseColor.WHITE);
-    }
-    //</editor-fold>    
+    //</editor-fold>  
 
     //<editor-fold defaultstate="collapsed" desc="addCell">
     void addCell(PdfPTable tb, String text, Font f, int align, BaseColor bg) {
@@ -235,13 +219,6 @@ public class PdfBuilder implements Serializable {
             cell.setHorizontalAlignment(align);
             tb.addCell(cell);
         }
-    }
-    //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="cellLayout">
-    //@Override
-    public void cellLayout(PdfPCell cell, Rectangle rectangle, PdfContentByte[] canvases) {
-
     }
     //</editor-fold>
 
@@ -264,19 +241,19 @@ public class PdfBuilder implements Serializable {
     }
     //</editor-fold>     
 
-    //<editor-fold defaultstate="collapsed" desc="createSignatureArea">
-    private void addSignatureArea(Document document) throws DocumentException {
-        PdfPTable unterschrift = new PdfPTable(2);
-        unterschrift.setWidths(new int[]{2, 1});
-        unterschrift.getDefaultCell().setBorder(Rectangle.NO_BORDER);
-        addLayoutCell(unterschrift, "für das Krankenhaus", SMALL, Element.ALIGN_LEFT);
-        addLayoutCell(unterschrift, "für die Sozialleistungsträger", SMALL, Element.ALIGN_LEFT);
-        addLayoutCell(unterschrift, "________________________________________________", SMALL, Element.ALIGN_LEFT);
+    //<editor-fold defaultstate="collapsed" desc="addSignaturArea">
+    private void addSignaturArea(Document document) throws DocumentException {
+        PdfPTable signaturArea = new PdfPTable(2);
+        signaturArea.setWidths(new int[]{2, 1});
+        signaturArea.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+        addLayoutCell(signaturArea, "für das Krankenhaus", SMALL, Element.ALIGN_LEFT);
+        addLayoutCell(signaturArea, "für die Sozialleistungsträger", SMALL, Element.ALIGN_LEFT);
+        addLayoutCell(signaturArea, "________________________________________________", SMALL, Element.ALIGN_LEFT);
         for (int i = 0; i < 6; i++) {
-            addLayoutCell(unterschrift, "______________________________", SMALL, Element.ALIGN_LEFT);
-            addLayoutCell(unterschrift, "", SMALL, Element.ALIGN_LEFT);
+            addLayoutCell(signaturArea, "______________________________", SMALL, Element.ALIGN_LEFT);
+            addLayoutCell(signaturArea, "", SMALL, Element.ALIGN_LEFT);
         }
-        document.add(unterschrift);
+        document.add(signaturArea);
     }
     //</editor-fold>    
 
@@ -322,54 +299,13 @@ public class PdfBuilder implements Serializable {
     //</editor-fold>    
 
     //<editor-fold defaultstate="collapsed" desc="addTableInfoandChecksum">
-    private void createTableInfoandChecksum(Document document, String fileName) throws NoSuchAlgorithmException, IOException, DocumentException {
-        //String checkSum = "Checksumme: " + toHex(Hash.MD5.checksum(file));
+    private void createTableInfoandChecksum(Document document , String s) throws NoSuchAlgorithmException, IOException, DocumentException {
         PdfPTable tb = new PdfPTable(new float[]{1, 1});
         tb.getDefaultCell().setBorder(Rectangle.NO_BORDER);
-        addLayoutCell(tb, "Anlage 1 zur Psych-Personalnachweisvereinbarung", SMALLBOLD, Element.ALIGN_LEFT);
-        addLayoutCell(tb, "Checksumme: " + getFileChecksum(MessageDigest.getInstance("MD5"),
-                new File(fileName)), SMALLBOLD, Element.ALIGN_LEFT);//SHA-1, MD5,SHA-512
+        addLayoutCell(tb, s + " zur Psych-Personalnachweisvereinbarung", SMALLBOLD, Element.ALIGN_LEFT);
+        addLayoutCell(tb, "Signatur: " + _editPsyStaff.getStaffProof().getChecksumAgreement(), SMALLBOLD, Element.ALIGN_LEFT);
         tb.setSpacingAfter(5);
         document.add(tb);
     }
     //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="toHex">
-    private static String toHex(byte[] bytes) {
-        return DatatypeConverter.printHexBinary(bytes);
-    }
-    //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="getFileChecksum">
-    private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
-        //Get file input stream for reading the file content
-        FileInputStream fis = new FileInputStream(file);
-
-        //Create byte array to read data in chunks
-        byte[] byteArray = new byte[1024];
-        int bytesCount = 0;
-
-        //Read file data and update in message digest
-        while ((bytesCount = fis.read(byteArray)) != -1) {
-            digest.update(byteArray, 0, bytesCount);
-        }
-
-        //close the stream; We don't need it now.
-        fis.close();
-
-        //Get the hash's bytes
-        byte[] bytes = digest.digest();
-
-        //This bytes[] has bytes in decimal format;
-        //Convert it to hexadecimal format
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < bytes.length; i++) {
-            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-        }
-
-        //return complete hash
-        return sb.toString();
-    }
-    //</editor-fold>
-
 }
