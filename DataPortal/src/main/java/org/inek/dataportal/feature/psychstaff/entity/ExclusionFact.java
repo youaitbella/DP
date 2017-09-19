f5a2d5b661bd7a4fe6f634d849de91dbbc8d07ff
Michael Müller <michael.mueller@inek-drg.de>
2017-09-19 14:34:47 +0200

    + ExclusionFact

---
+++ DataPortal/src/main/java/org/inek/dataportal/feature/psychstaff/entity/ExclusionFact.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inek.dataportal.feature.psychstaff.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import org.inek.dataportal.utils.Documentation;
+
+/**
+ *
+ * @author muellermi
+ */
+@Entity
+@Table(name = "listExclusionFact", schema = "psy")
+public class ExclusionFact implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    //<editor-fold defaultstate="collapsed" desc="Property Id">
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "efId")
+    private int _id;
+    
+    public int getId() {
+        return _id;
+    }
+    
+    public void setId(int id) {
+        this._id = id;
+    }
+    //</editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Property Name">
+    @Column(name = "efName")
+    @Documentation(key = "lblName")
+    private String _name = "";
+
+    @Size(max = 50)
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+    // </editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Property YearFrom">
+    @Id
+    @Column(name = "efYearFrom")
+    private int _yearTo;
+    
+    public int getYearTo() {
+        return _yearTo;
+    }
+    
+    public void setYearTo(int yearTo) {
+        this._yearTo = yearTo;
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="hashCode, equals, toString">
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this._yearTo;
+        hash = 97 * hash + Objects.hashCode(this._name);
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ExclusionFact other = (ExclusionFact) obj;
+        if (this._yearTo != other._yearTo) {
+            return false;
+        }
+        if (!Objects.equals(this._name, other._name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return _name;
+    }
+    //</editor-fold>
+    
+}
