4c32dc95a6bbf8f84d10936e1146865fedbfa05c
Michael Müller <michael.mueller@inek-drg.de>
2018-01-18 11:48:05 +0100

    add AccountService

--- AccountService/src/main/java/org/inek/accountservice/ApplicationConfig.java
+++ AccountService/src/main/java/org/inek/accountservice/ApplicationConfig.java
@@ -25,6 +25,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(
             Set<Class<?>> resources) {
+        resources.add(org.inek.accountservice.AccountREST.class);
     }
     
 }
