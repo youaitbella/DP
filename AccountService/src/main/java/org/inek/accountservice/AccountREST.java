cc88587794fe30b292c5948ab6166908928ebbb7
Michael Müller <michael.mueller@inek-drg.de>
2018-01-22 17:28:22 +0100

    testing a valid method to pass token

--- AccountService/src/main/java/org/inek/accountservice/AccountREST.java
+++ AccountService/src/main/java/org/inek/accountservice/AccountREST.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.ejb.Schedule;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
