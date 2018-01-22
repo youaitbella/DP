b3f70232242136415ca6cc570ae09c732151e4b4
Michael Müller <michael.mueller@inek-drg.de>
2018-01-22 17:41:50 +0100

    add a valid method to pass token

--- DataPortal/src/main/java/org/inek/dataportal/common/RequestController.java
+++ DataPortal/src/main/java/org/inek/dataportal/common/RequestController.java
@@ -39,7 +39,7 @@ public class RequestController implements Serializable {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         String viewId = facesContext.getViewRoot().getViewId();
         if (loginByToken(facesContext)) {
-            //facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, viewId);
+            facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, Pages.MainApp.RedirectURL());
             return;
         }
         if (_sessionController.isLoggedIn()) {
