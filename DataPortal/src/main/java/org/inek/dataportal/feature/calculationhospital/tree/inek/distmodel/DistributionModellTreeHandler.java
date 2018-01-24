539f107a39a301e3314f6844b82a08eb37c316d4
Michael Müller <michael.mueller@inek-drg.de>
2018-01-24 09:37:42 +0100

    CalcDistTreeHandler: extract TreeNodeObserver

--- DataPortal/src/main/java/org/inek/dataportal/feature/calculationhospital/tree/inek/distmodel/DistributionModellTreeHandler.java
+++ DataPortal/src/main/java/org/inek/dataportal/feature/calculationhospital/tree/inek/distmodel/DistributionModellTreeHandler.java
@@ -9,7 +9,9 @@ import java.util.Optional;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.inek.dataportal.common.ApplicationTools;
@@ -30,17 +32,19 @@ import org.inek.portallib.tree.TreeNodeObserver;
  * @author muellermi
  */
 @Named @SessionScoped
-public class DistributionModellTreeHandler implements Serializable, TreeNodeObserver {
+public class DistributionModellTreeHandler implements Serializable {
 
     private static final Logger LOGGER = Logger.getLogger("DistributionModellTreeHandler");
     private static final long serialVersionUID = 1L;
 
-    @Inject private DistributionModelFacade _distributionModelFacade;
-    @Inject private SessionController _sessionController;
-    @Inject private ApplicationTools _appTools;
+    @Inject private Instance<RootTreeNodeObserver> _rootTreeNodeObserverProvider;
 
-    private final RootNode _rootNode = RootNode.create(0, this);
-    private AccountTreeNode _accountNode;
+    private RootNode _rootNode;
+
+    @PostConstruct
+    private void init() {
+        _rootNode = RootNode.create(0, _rootTreeNodeObserverProvider.get());
+    }
 
     public RootNode getRootNode() {
         if (!_rootNode.isExpanded()) {
@@ -75,81 +79,4 @@ public class DistributionModellTreeHandler implements Serializable, TreeNodeObse
         refreshNodes();
     }
 
-    @Override
-    public Collection<TreeNode> obtainChildren(TreeNode treeNode) {
-        if (treeNode instanceof RootNode) {
-            return obtainRootNodeChildren((RootNode) treeNode);
-        }
-        if (treeNode instanceof AccountTreeNode) {
-            return obtainAccountNodeChildren((AccountTreeNode) treeNode);
-        }
-        return new ArrayList<TreeNode>();
-    }
-
-    private Collection<TreeNode> obtainRootNodeChildren(RootNode treeNode) {
-        List<Account> accounts = _distributionModelFacade.getInekAccounts(getYear(), getFilter());
-        Account currentUser = _sessionController.getAccount();
-        if (accounts.contains(currentUser)) {
-            // ensure current user is first, if in list
-            accounts.remove(currentUser);
-            accounts.add(0, currentUser);
-        }
-        List<? extends TreeNode> oldChildren = new ArrayList<>(treeNode.getChildren());
-        Collection<TreeNode> children = new ArrayList<>();
-        for (Account account : accounts) {
-            Integer id = account.getId();
-            Optional<? extends TreeNode> existing = oldChildren.stream().filter(n -> n.getId() == id).findFirst();
-            AccountTreeNode childNode = existing.isPresent() ? (AccountTreeNode) existing.get() : AccountTreeNode.
-                    create(treeNode, account, this);
-            children.add((TreeNode) childNode);
-            oldChildren.remove(childNode);
-            if (currentUser.equals(account) || accounts.size() <= 3) {
-                childNode.expand();  // auto-expand own node
-            }
-        }
-        return children;
-    }
-
-    private Collection<TreeNode> obtainAccountNodeChildren(AccountTreeNode treeNode) {
-        List<CalcHospitalInfo> infos = _distributionModelFacade.
-                getDistributionModelsByEmail(treeNode.getEmail(), getYear(), getFilter());
-        Collection<TreeNode> children = new ArrayList<>();
-        for (CalcHospitalInfo info : infos) {
-            children.add(CalcHospitalTreeNode.create(treeNode, info, this));
-        }
-        return children;
-    }
-
-    @Override
-    public Collection<TreeNode> obtainSortedChildren(TreeNode treeNode) {
-        if (treeNode instanceof AccountTreeNode) {
-            return sortAccountNodeChildren((AccountTreeNode) treeNode);
-        }
-        return treeNode.getChildren();
-    }
-
-    public Collection<TreeNode> sortAccountNodeChildren(AccountTreeNode treeNode) {
-        Stream<CalcHospitalTreeNode> stream = treeNode.getChildren().stream().map(n -> (CalcHospitalTreeNode) n);
-        Stream<CalcHospitalTreeNode> sorted;
-        int direction = treeNode.isDescending() ? -1 : 1;
-        switch (treeNode.getSortCriteria().toLowerCase()) {
-            case "ik":
-                sorted = stream.sorted((n1, n2) -> direction * Integer.compare(n1.getCalcHospitalInfo().getIk(), n2.
-                        getCalcHospitalInfo().getIk()));
-                break;
-            case "hospital":
-                sorted = stream.sorted((n1, n2) -> direction * _appTools.retrieveHospitalInfo(n1.getCalcHospitalInfo().
-                        getIk())
-                        .compareTo(_appTools.retrieveHospitalInfo(n2.getCalcHospitalInfo().getIk())));
-                break;
-            case "name":
-                sorted = stream.sorted((n1, n2) -> direction * n1.getCalcHospitalInfo().getName().compareTo(n2.
-                        getCalcHospitalInfo().getName()));
-                break;
-            case "status":
-            default:
-                sorted = stream;
-        }
-        return sorted.collect(Collectors.toList());
-    }
 }
