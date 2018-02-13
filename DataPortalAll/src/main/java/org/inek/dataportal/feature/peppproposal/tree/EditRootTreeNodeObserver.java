8654f0122288d2082d67851e4ce545e687c4e4fa
Michael Müller <michael.mueller@inek-drg.de>
2018-02-13 08:42:57 +0100

    PEPP: fix tree handling

--- DataPortalAll/src/main/java/org/inek/dataportal/feature/peppproposal/tree/EditRootTreeNodeObserver.java
+++ DataPortalAll/src/main/java/org/inek/dataportal/feature/peppproposal/tree/EditRootTreeNodeObserver.java
@@ -21,7 +21,6 @@ import org.inek.dataportal.common.enums.Feature;
 import org.inek.dataportal.common.enums.WorkflowStatus;
 import org.inek.dataportal.facades.PeppProposalFacade;
 import org.inek.dataportal.facades.account.AccountFacade;
-import org.inek.dataportal.common.tree.RootNode;
 import org.inek.dataportal.helper.tree.entityTree.AccountTreeNode;
 import org.inek.dataportal.common.tree.TreeNode;
 import org.inek.dataportal.common.tree.TreeNodeObserver;
--- DataPortalAll/src/main/java/org/inek/dataportal/feature/peppproposal/tree/PeppProposalTreeHandler.java
+++ DataPortalAll/src/main/java/org/inek/dataportal/feature/peppproposal/tree/PeppProposalTreeHandler.java
@@ -2,25 +2,19 @@ package org.inek.dataportal.feature.peppproposal.tree;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
-import org.inek.dataportal.common.AccessManager;
 import org.inek.dataportal.controller.SessionController;
 import org.inek.dataportal.entities.pepp.PeppProposal;
 import org.inek.dataportal.enums.Pages;
 import org.inek.dataportal.facades.PeppProposalFacade;
-import org.inek.dataportal.facades.account.AccountFacade;
 import org.inek.dataportal.helper.Utils;
 import org.inek.dataportal.helper.tree.entityTree.AccountTreeNode;
 import org.inek.dataportal.helper.tree.ProposalInfoTreeNode;
@@ -38,7 +32,6 @@ import org.inek.dataportal.utils.KeyValueLevel;
 @SessionScoped
 public class PeppProposalTreeHandler implements Serializable {
 
-    private static final Logger LOGGER = Logger.getLogger("PeppProposalTreeHandler");
     private static final long serialVersionUID = 1L;
 
     @Inject private PeppProposalFacade _peppProposalFacade;
