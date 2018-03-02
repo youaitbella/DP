f2a1b91bf4ea148880863b178f9cc379fa4527d4
Michael Müller <michael.mueller@inek-drg.de>
2018-03-02 14:59:37 +0100

    re-implement changeNubOwner and remove nub from admin

--- DataPortalAdmin/src/main/java/org/inek/dataportal/feature/admin/facade/AdminFacade.java
+++ DataPortalAdmin/src/main/java/org/inek/dataportal/feature/admin/facade/AdminFacade.java
@@ -5,13 +5,18 @@
  */
 package org.inek.dataportal.feature.admin.facade;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import org.inek.dataportal.common.data.AbstractDataAccess;
+import org.inek.dataportal.common.data.account.entities.Account;
 import org.inek.dataportal.common.data.adm.ReportTemplate;
+import org.inek.dataportal.feature.admin.backingbean.AccountInfo;
 
 /**
  * Hides the database accesses fro the admin tasks behind a facade
@@ -21,6 +26,8 @@ import org.inek.dataportal.common.data.adm.ReportTemplate;
 @RequestScoped
 @Transactional
 public class AdminFacade extends AbstractDataAccess {
+
+    private static final String IK = "ik";
 
     public ReportTemplate findReportTemplate(int id) {
         return getEntityManager().find(ReportTemplate.class, id);
@@ -37,5 +44,44 @@ public class AdminFacade extends AbstractDataAccess {
             return Optional.empty();
         }
     }
-   
+
+    public List<AccountInfo> retrieveNubAccountInfos(int ik) {
+        String jpql = "SELECT a, false, count(n) FROM NubRequest n JOIN Account a WHERE n._accountId = a._id and n._ik = :ik GROUP BY a";
+        // sadly this is not a list of the expected type, but of object[]
+//        TypedQuery<AccountInfo> query = getEntityManager().createQuery(jpql, AccountInfo.class);
+//        query.setParameter("ik", ik);
+//        List<AccountInfo> infos = query.getResultList();
+//        return infos;
+
+        // although the compiler tells us something else, this is what we get
+        List<AccountInfo> infos = new ArrayList<>();
+        Query query = getEntityManager().createQuery(jpql);
+        query.setParameter(IK, ik);
+        @SuppressWarnings("unchecked") List<Object[]> objects = query.getResultList();
+        for (Object[] obj : objects) {
+            AccountInfo info = new AccountInfo((Account) obj[0], (boolean) obj[1], (int) (long) obj[2]);
+            infos.add(info);
+        }
+        return infos;
+    }
+
+    public void changeNubOwner(int ik, int oldAccountId, int newAccountId, int agentAccountId) {
+        String sql = ("begin transaction;\n"
+                + "\n"
+                + "insert into adm.Log (logTS, logAccountId, logSession, logMessage )\n"
+                + "select getDate(), {agentAccountId}, 'internal', "
+                + "'NubOwner changed: nub=' + cast(nubId as varchar) + ', oldOwner={oldAccountId}, newOwner={newAccountId}'\n"
+                + "from  dbo.NubProposal where nubAccountId = {oldAccountId} and nubIk = {ik};\n"
+                + "\n"
+                + "update dbo.NubProposal set nubAccountId = {newAccountId} where nubIk = {ik} and nubAccountId = {oldAccountId};\n"
+                + "\n"
+                + "commit transaction;")
+                        .replace("{ik}", "" + ik)
+                        .replace("{oldAccountId}", "" + oldAccountId)
+                        .replace("{newAccountId}", "" + newAccountId)
+                        .replace("{agentAccountId}", "" + agentAccountId);
+        Query query = getEntityManager().createNativeQuery(sql);
+        query.executeUpdate();
+        clearCache();
+    }
 }
