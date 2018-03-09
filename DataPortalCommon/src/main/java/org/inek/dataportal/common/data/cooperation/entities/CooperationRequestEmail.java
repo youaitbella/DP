9fe7dc6221432098a8acad4c88f63754ea35f531
Michael Müller <michael.mueller@inek-drg.de>
2018-03-09 11:13:03 +0100

    cooperation: add missing creationDate

--- DataPortalCommon/src/main/java/org/inek/dataportal/common/data/cooperation/entities/CooperationRequestEmail.java
+++ DataPortalCommon/src/main/java/org/inek/dataportal/common/data/cooperation/entities/CooperationRequestEmail.java
@@ -1,6 +1,7 @@
 package org.inek.dataportal.common.data.cooperation.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.*;
 
 /**
@@ -21,40 +22,61 @@ public class CooperationRequestEmail implements Serializable {
         return cre;
     }
     
+    //<editor-fold defaultstate="collapsed" desc="Property Id">
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "creId")
     private Integer _id;
-
-    @Column(name = "creCreatorId")
-    private int _creatorId;
-
-    @Column(name = "creRequestEmail")
-    private String _requestEmail;
-
     public Integer getId() {
         return _id;
     }
-
+    
     public void setId(Integer id) {
-        this._id = id;
+        _id = id;
     }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Property CreatorId">
+    @Column(name = "creCreatorId")
+    private int _creatorId;
 
     public int getCreatorId() {
         return _creatorId;
     }
 
     public void setCreatorId(int creatorId) {
-        this._creatorId = creatorId;
+        _creatorId = creatorId;
     }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="RequestEmail">
+    @Column(name = "creRequestEmail")
+    private String _requestEmail;
 
     public String getRequestEmail() {
         return _requestEmail;
     }
 
     public void setRequestEmail(String requestEmail) {
-        this._requestEmail = requestEmail;
+        _requestEmail = requestEmail;
     }
+    //</editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Property CreationDate">
+    @Column(name = "creCreated")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date _creationDate = new Date();
+
+    public Date getCreationDate() {
+        return _creationDate;
+    }
+
+    public void setCreationDate(Date value) {
+        _creationDate = value;
+    }
+    // </editor-fold>
+    
+    
     // <editor-fold defaultstate="collapsed" desc="hashCode / equals / toString">
     @Override
     public int hashCode() {
