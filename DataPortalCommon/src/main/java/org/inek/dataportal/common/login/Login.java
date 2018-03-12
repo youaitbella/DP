5d5be9703e86443bd72ab00718aa8db7902f6c3b
Michael Müller <michael.mueller@inek-drg.de>
2018-03-12 17:22:03 +0100

    login

--- DataPortalCommon/src/main/java/org/inek/dataportal/common/login/Login.java
+++ DataPortalCommon/src/main/java/org/inek/dataportal/common/login/Login.java
@@ -85,7 +85,9 @@ public class Login implements Serializable {
             return "";
         }
         _loginMessage = "";
-        return _sessionController.countInstalledFeatures() <= 1 ? Pages.UserMaintenanceFeatures.URL() : Pages.MainApp.URL();
+        // todo: check for features and call maintenace dialog???
+        //return _sessionController.countInstalledFeatures() <= 1 ? Pages.UserMaintenanceFeatures.URL() : Pages.MainApp.URL();
+        return Pages.MainApp.URL();
     }
 
 }
