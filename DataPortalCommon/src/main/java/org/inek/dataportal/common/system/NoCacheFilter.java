0acb7e9cb3ae493f53d403688ddfeb8154990c7c
Michael Müller <michael.mueller@inek-drg.de>
2018-03-21 10:55:09 +0100

    move noCachefilter to common

--- DataPortalCommon/src/main/java/org/inek/dataportal/common/system/NoCacheFilter.java
+++ DataPortalCommon/src/main/java/org/inek/dataportal/common/system/NoCacheFilter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.inek.dataportal.system;
+package org.inek.dataportal.common.system;
 
 import java.io.IOException;
 import javax.faces.application.ResourceHandler;
