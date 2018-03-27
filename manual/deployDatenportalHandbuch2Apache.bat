adee9ac339f087878b044217d420b979527f8ad7
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2018-03-27 11:04:44 +0200

    manuel - add scripts for deploy on server

---
+++ manual/deployDatenportalHandbuch2Apache.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo Kopiere InEK-Datenportal.pdf auf den Server
+scp ../DataPortal/src/main/webapp/resources/manual/InEK-Datenportal.pdf edv@vDmzApacheSvr01:.
+echo.
+
+echo Starte deployment auf dem Server
+ssh edv@vDmzApacheSvr01 /srv/scripte/deployDatenportalHandbuch.sh
+echo.
+
+echo Fertig
+echo.
