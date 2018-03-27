a65538f42509e3b501eb98655e48a05595b75613
Michael Müller <michael.mueller@inek-drg.de>
2018-03-27 08:54:13 +0200

    prepare calc app

---
+++ DataPortalCalc/src/main/java/org/inek/dataportal/calc/controller/FeatureFactoryImpl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inek.dataportal.calc.controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.Dependent;
+import org.inek.dataportal.common.controller.FeatureFactory;
+import org.inek.dataportal.common.controller.IFeatureController;
+import org.inek.dataportal.common.controller.SessionController;
+import org.inek.dataportal.common.enums.Feature;
+
+/**
+ *
+ * @author muellermi
+ */
+@Dependent
+public class FeatureFactoryImpl implements FeatureFactory, Serializable{
+
+    @Override
+    public IFeatureController createController(Feature feature, SessionController sessionController) {
+
+        switch (feature) {
+            default:
+                throw new IllegalArgumentException("no such controller");
+        }
+    }
+}
