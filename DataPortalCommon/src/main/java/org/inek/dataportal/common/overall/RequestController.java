23deafe2af6981d0b6d9de97f00cb6fec4b98eda
Michael Müller <michael.mueller@inek-drg.de>
2018-04-10 16:54:48 +0200

    re-activate NoCache filter

--- DataPortalCommon/src/main/java/org/inek/dataportal/common/overall/RequestController.java
+++ DataPortalCommon/src/main/java/org/inek/dataportal/common/overall/RequestController.java
@@ -8,15 +8,12 @@ import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.ResourceHandler;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ComponentSystemEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.inek.dataportal.common.controller.SessionController;
 import org.inek.dataportal.common.enums.Pages;
 import org.inek.dataportal.common.enums.PortalType;
@@ -48,7 +45,6 @@ public class RequestController implements Serializable {
             return;
         }
         if (_sessionController.isLoggedIn()) {
-            AddCacheControlToResponse(facesContext);
             return;
         }
         _sessionController.logMessage("Force to login: IP=" + Utils.getClientIP() + "; FromView=" + viewId);
@@ -58,21 +54,6 @@ public class RequestController implements Serializable {
             facesContext.getExternalContext().redirect(url);
         } catch (Exception ex) {
             LOGGER.log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private void AddCacheControlToResponse(FacesContext facesContext) {
-        if (facesContext.getPartialViewContext().isPartialRequest()) {
-            return;
-        }
-        ExternalContext externalContext = facesContext.getExternalContext();
-        HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
-        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-        if (!response.containsHeader("Content-Disposition")
-                && !request.getRequestURI().contains(ResourceHandler.RESOURCE_IDENTIFIER)) { // Skip JSF resources (CSS/JS/Images/etc)
-            response.setHeader("Cache-Control", "no-store, must-revalidate"); // HTTP 1.1. without no-cache!
-            response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
-            response.setDateHeader("Expires", 0); // Proxies.
         }
     }
 
