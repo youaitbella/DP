174758b393a811dcb15efe2525ccc51b12dbc09b
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2018-05-15 10:04:21 +0200

    prepare test for calc

---
+++ DataPortalCalc/src/test/java/org/inek/dataportal/calc/facades/CalcFacadeTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inek.dataportal.calc.facades;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lautenti
+ */
+public class CalcFacadeTest {
+
+    public CalcFacadeTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
