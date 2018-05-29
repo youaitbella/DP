da158737b372fc65bfce8f521469662042f65170
Michael Müller <michael.mueller@inek-drg.de>
2018-05-29 08:44:23 +0200

    configure junit5

--- DataPortalBase/src/test/java/org/inek/dataportal/base/feature/ikadmin/backingbean/IkAdminTasksTest.java
+++ DataPortalBase/src/test/java/org/inek/dataportal/base/feature/ikadmin/backingbean/IkAdminTasksTest.java
@@ -1,10 +1,5 @@
 package org.inek.dataportal.base.feature.ikadmin.backingbean;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.util.ArrayList;
 import java.util.List;
