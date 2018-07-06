998606753609590bfc34af97e03acad21b1e34c0
Michael Müller <michael.mueller@inek-drg.de>
2018-07-06 09:16:46 +0200

    fix var scope, small refacts

--- DataPortalAdmin/src/main/java/org/inek/dataportal/admin/backingbean/AdminInfoText.java
+++ DataPortalAdmin/src/main/java/org/inek/dataportal/admin/backingbean/AdminInfoText.java
@@ -6,19 +6,12 @@
 package org.inek.dataportal.admin.backingbean;
 
 import java.io.Serializable;
-
 import java.util.List;
-
 import javax.annotation.PostConstruct;
-
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import org.inek.dataportal.common.controller.SessionController;
-
 import org.inek.dataportal.common.data.infotext.entity.InfoText;
 import org.inek.dataportal.common.data.infotext.facade.InfoTextFacade;
-
 import org.inek.dataportal.common.scope.FeatureScoped;
 
 /**
@@ -31,27 +24,21 @@ public class AdminInfoText implements Serializable {
 
     @Inject
     private InfoTextFacade _infoTextFacade;
-    @Inject
-    private SessionController _sessionController;
 
     //<editor-fold defaultstate="collapsed" desc="List of Info Texts">
     private List<InfoText> _listOfInfoTexts;
 
     public void setListOfInfoTexts(List<InfoText> listOfInfoTexts) {
-
         _listOfInfoTexts = listOfInfoTexts;
-
     }
 
     public List<InfoText> getListOfInfoTexts() {
-
         return _listOfInfoTexts;
-
     }
-
     //</editor-fold>
+
     //<editor-fold defaultstate="collapsed" desc="New Key Name">
-    String _newKey;
+    private String _newKey;
 
     public String getNewKey() {
         return _newKey;
@@ -60,14 +47,12 @@ public class AdminInfoText implements Serializable {
     public void setNewKey(String newKey) {
         this._newKey = newKey;
     }
-
     //</editor-fold>
+
     //<editor-fold defaultstate="collapsed" desc="Drop Down Text">
     @PostConstruct
     private void initData() {
-
         _listOfInfoTexts = _infoTextFacade.getAllInfoTexts("DE");
-
     }
 
     public void addInfoText(String key) {
@@ -79,16 +64,12 @@ public class AdminInfoText implements Serializable {
 
     }
 
-    public void onSave() {
+    public void save() {
 
         for (InfoText infoText : _listOfInfoTexts) {
-
             if (infoText.getModified()) {
-
                 _infoTextFacade.save(infoText);
-
             }
-
         }
 
     }
