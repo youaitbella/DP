e4c852569e0eece176f2f6323136a68a6cfe5488
Marius Lanzrath <Marius.Lanzrath@inek-drg.de>
2018-07-11 16:47:15 +0200

    Created Backingbeans for Mail Template and System Status

--- DataPortalAdmin/src/main/java/org/inek/dataportal/admin/backingbean/AdminSystemStatus.java
+++ DataPortalAdmin/src/main/java/org/inek/dataportal/admin/backingbean/AdminSystemStatus.java
@@ -5,10 +5,62 @@
  */
 package org.inek.dataportal.admin.backingbean;
 
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.inek.dataportal.admin.system.SessionCounter;
+import org.inek.dataportal.api.enums.Feature;
+import org.inek.dataportal.common.controller.SessionController;
+import org.inek.dataportal.common.data.account.facade.AccountFacade;
+import org.inek.dataportal.common.enums.Pages;
+import org.inek.dataportal.common.helper.Utils;
+import org.inek.dataportal.common.scope.FeatureScoped;
+
 /**
  *
  * @author lanzrama
  */
-public class AdminSystemStatus {
+@Named
+@FeatureScoped
+public class AdminSystemStatus implements Serializable {
+
+    @Inject
+    private AccountFacade _accountFacade;
+    @Inject
+    private SessionController _sessionController;
+
+    @PostConstruct
+    private void init() {
+        if (!_sessionController.isInekUser(Feature.ADMIN)) {
+            _sessionController.logMessage("Non-authorized access to admin task.");
+            FacesContext fc = FacesContext.getCurrentInstance();
+            NavigationHandler nav = fc.getApplication().getNavigationHandler();
+            nav.handleNavigation(fc, null, Pages.NotAllowed.URL());
+        }
+    }
+
+    public int getSessionCount() {
+        return SessionCounter.getCount();
+    }
+
+    public void clearCache() {
+        _accountFacade.clearCache();
+        Utils.showMessageInBrowser("Cache cleared.");
+    }
+
+    public String getProcessorInfo() {
+        Runtime runtime = Runtime.getRuntime();
+        return "Processor cores " + runtime.availableProcessors();
+    }
+
+    public String getMemoryInfo() {
+        int MB = 1024 * 1024;
+        Runtime runtime = Runtime.getRuntime();
+        return "Speicher: max " + runtime.maxMemory() / MB + ", total " + runtime.totalMemory() / MB + "; free " + runtime.
+                freeMemory() / MB;
+    }
 
 }
