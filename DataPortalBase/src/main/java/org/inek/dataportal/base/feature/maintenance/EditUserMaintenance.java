68d364f54c3304fff062a47c2e4be2f5fd07e64b
Michael Müller <michael.mueller@inek-drg.de>
2018-08-30 15:36:53 +0200

    deploy AllToProduction

--- DataPortalBase/src/main/java/org/inek/dataportal/base/feature/maintenance/EditUserMaintenance.java
+++ DataPortalBase/src/main/java/org/inek/dataportal/base/feature/maintenance/EditUserMaintenance.java
@@ -381,7 +381,8 @@ public class EditUserMaintenance extends AbstractEditController {
                         && _ikAdminFacade.findAccessRightsByAccountIkAndFeature(account, ik, feature.getFeature()).
                                 isEmpty()) {
                     AccessRight accessRight = new AccessRight(account.getId(), ik, feature.getFeature(), _ikAdminFacade.
-                            hasIkAdmin(ik) ? Right.Deny : Right.All);  // todo: if InEK does not approve features anymor but rights, then chang to Deny always
+                            hasIkAdmin(ik) ? Right.Deny : Right.All);  
+        // todo: if InEK does not approve features anymor but rights, then chang to Deny always
                     _ikAdminFacade.saveAccessRight(accessRight);
                     hasNewEntry = true;
                 }
