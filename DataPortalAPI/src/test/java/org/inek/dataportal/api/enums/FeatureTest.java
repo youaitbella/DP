d7fdcc4df5251b9c9df4a079099b743567641486
Michael Müller <michael.mueller@inek-drg.de>
2018-08-31 13:43:16 +0200

    add test for Feature

--- DataPortalAPI/src/test/java/org/inek/dataportal/api/enums/FeatureTest.java
+++ DataPortalAPI/src/test/java/org/inek/dataportal/api/enums/FeatureTest.java
@@ -1,5 +1,7 @@
 package org.inek.dataportal.api.enums;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,10 +15,14 @@ public class FeatureTest {
 
     @Test
     public void featureFromIdReturnsFeatureForValidId() {
+        Feature result = Feature.getFeatureFromId(1);
+        assertThat(result).isEqualTo(Feature.ADMIN);
     }
     
     @Test
     public void featureFromIdThrowsExceptionForInvalidId() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Feature.getFeatureFromId(-1));
+        assertThat(exception.getMessage()).isEqualTo("Failed to obtain feature. Unknown id -1");
     }
     
 }
