9641d070790091770697bd5b6c0771e40a86e160
Michael Müller <michael.mueller@inek-drg.de>
2018-08-31 12:06:42 +0200

    prepare feature test

---
+++ DataPortalAPI/src/test/java/org/inek/dataportal/api/enums/FeatureTest.java
@@ -0,0 +1,22 @@
+package org.inek.dataportal.api.enums;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author muellermi
+ */
+public class FeatureTest {
+    
+    public FeatureTest() {
+    }
+
+    @Test
+    public void featureFromIdReturnsFeatureForValidId() {
+    }
+    
+    @Test
+    public void featureFromIdThrowsExceptionForInvalidId() {
+    }
+    
+}
