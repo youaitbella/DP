56a2971903c36ab6c38ed784fdc63a06607aba3e
Michael Müller <michael.mueller@inek-drg.de>
2018-10-11 18:18:10 +0200

    test

--- DataPortalCert/src/test/java/org/inek/dataportal/cert/Helper/LetterConverterTest.java
+++ DataPortalCert/src/test/java/org/inek/dataportal/cert/Helper/LetterConverterTest.java
@@ -12,7 +12,7 @@ public class LetterConverterTest {
     @Test
     public void describeLettersReturnsString() {
         List<String> result = LetterConverter.describeLetters("T3t!");
-        assertThat(result).isNotNull().isNotEmpty().containsExactly("T = Theodor", "3 = Drei", "T = Theodor (klein)", "! = Ausrufungszeichen");
+        assertThat(result).isNotNull().isNotEmpty().containsOnly("T = Theodor", "3 = Drei", "T = Theodor (klein)", "! = Ausrufungszeichen");
     }
     
 }
