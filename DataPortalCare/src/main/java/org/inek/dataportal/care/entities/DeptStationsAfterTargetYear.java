96bd167fc753171e603bff2554fd39814a88c8ed
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2018-10-15 10:51:00 +0200

    fix checkstyle

--- DataPortalCare/src/main/java/org/inek/dataportal/care/entities/DeptStationsAfterTargetYear.java
+++ DataPortalCare/src/main/java/org/inek/dataportal/care/entities/DeptStationsAfterTargetYear.java
@@ -176,6 +176,7 @@ public class DeptStationsAfterTargetYear implements Serializable {
     //</editor-fold>
 
 
+    @SuppressWarnings("CyclomaticComplexity")
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
