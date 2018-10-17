2a6f038fdd5f16ed8a8a04735790babc68f8c3c4
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2018-10-17 07:57:54 +0200

    care: change excel exporter

--- DataPortalCare/src/main/java/org/inek/dataportal/care/utils/CareExcelExporter.java
+++ DataPortalCare/src/main/java/org/inek/dataportal/care/utils/CareExcelExporter.java
@@ -76,8 +76,13 @@ public class CareExcelExporter {
 
     private void setBaseInformations(Sheet sheet, DeptBaseInformation baseInfo, String hospitalName, String hospitalTown) {
 
+        //ik Cell style
+        CellStyle styleIk = sheet.getWorkbook().createCellStyle();
+        styleIk.setAlignment(HorizontalAlignment.LEFT);
+
         sheet.createRow(0).createCell(0).setCellValue(IK);
         sheet.getRow(0).createCell(1).setCellValue(new Double(baseInfo.getIk()));
+        sheet.getRow(0).getCell(1).setCellStyle(styleIk);
 
         sheet.createRow(1).createCell(0).setCellValue(KH_NAME);
         sheet.getRow(1).createCell(1).setCellValue(hospitalName);
@@ -89,6 +94,7 @@ public class CareExcelExporter {
         CellStyle cellStyle = sheet.getWorkbook().createCellStyle();
         CreationHelper createHelper = sheet.getWorkbook().getCreationHelper();
         cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd/MM/yyyy"));
+        cellStyle.setAlignment(HorizontalAlignment.LEFT);
 
         sheet.createRow(4).createCell(0).setCellValue(SEND_AT);
         sheet.getRow(4).createCell(1).setCellValue(baseInfo.getSend());
@@ -101,6 +107,8 @@ public class CareExcelExporter {
         Font font = sheet.getWorkbook().createFont();
         font.setBold(true);
         style.setFont(font);
+        style.setAlignment(HorizontalAlignment.LEFT);
+
         sheet.getRow(0).getCell(0).setCellStyle(style);
         sheet.getRow(1).getCell(0).setCellStyle(style);
         sheet.getRow(2).getCell(0).setCellStyle(style);
@@ -113,6 +121,8 @@ public class CareExcelExporter {
         Font font = sheet.getWorkbook().createFont();
         font.setBold(true);
         style.setFont(font);
+        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
         Row row = sheet.createRow(6);
         row.createCell(0).setCellValue(TABLE_HEADER_1);
@@ -195,6 +205,8 @@ public class CareExcelExporter {
         int startRow = 6;
 
         CellStyle style = getCellStyleBorder(sheet);
+        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
         Font font = sheet.getWorkbook().createFont();
         font.setBold(true);
