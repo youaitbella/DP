f6943647f5a6840438b33fd966432317a50bde68
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2018-11-02 10:54:20 +0100

    supress warning

--- DataPortalCert/src/main/java/org/inek/dataportal/cert/Helper/LetterConverter.java
+++ DataPortalCert/src/main/java/org/inek/dataportal/cert/Helper/LetterConverter.java
@@ -14,6 +14,7 @@ import java.util.Map;
  *
  * @author palenma
  */
+@SuppressWarnings("JavaNCSS")
 public class LetterConverter {
 
     private static final Map<String, String> CHAR_2_WORD = new HashMap<>();
