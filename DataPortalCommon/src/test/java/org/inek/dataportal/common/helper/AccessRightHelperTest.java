2c3086700bba1ad72c5f669a167b4cfb0d801af0
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2019-01-03 08:20:31 +0100

    adding test / refactor #147

--- DataPortalCommon/src/test/java/org/inek/dataportal/common/helper/AccessRightHelperTest.java
+++ DataPortalCommon/src/test/java/org/inek/dataportal/common/helper/AccessRightHelperTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.inek.dataportal.api.enums.Feature;
 import org.inek.dataportal.common.data.account.entities.Account;
 import org.inek.dataportal.common.data.ikadmin.entity.IkAdmin;
+import org.inek.dataportal.common.enums.Right;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -14,33 +15,100 @@ import java.util.Set;
 class AccessRightHelperTest {
 
     @Test
-    public void ensureRightsForAccountFeatureTest() {
-        Account acc = new Account();
+    public void ensureRightsForAccountFeatureWithNonExistsRightsTest() {
+        Account acc = createNewAccount(2);
         acc.addFeature(Feature.NUB, true);
         acc.addFeature(Feature.CALCULATION_HOSPITAL, true);
         int ik = 222222222;
-        List<Feature> features = new ArrayList<>();
-        features.add(Feature.NUB);
-        features.add(Feature.CALCULATION_HOSPITAL);
+        List<Feature> features1 = new ArrayList<>();
+        features1.add(Feature.NUB);
+
+        List<Feature> features2 = new ArrayList<>();
+        features2.add(Feature.CALCULATION_HOSPITAL);
 
         List<IkAdmin> ikAdminsForIk = new ArrayList<>();
-        ikAdminsForIk.add(createNewIkAdmin(new Account(), ik, features));
+        ikAdminsForIk.add(createNewIkAdmin(createNewAccount(1), ik, features1));
+        ikAdminsForIk.add(createNewIkAdmin(createNewAccount(3), ik, features2));
 
         Set<Integer> ikAdminsForEmailing = new HashSet<>();
 
         AccessRightHelper.ensureRightsForAccountFeature(acc, ikAdminsForIk, ikAdminsForEmailing, ik);
 
         Assertions.assertThat(acc.getAccessRights()).hasSize(2);
+        Assertions.assertThat(acc.getAccessRights().stream().filter(c -> c.getFeature() == Feature.NUB).findFirst()
+                .get().getRight()).isEqualTo(Right.Deny);
+        Assertions.assertThat(acc.getAccessRights().stream().filter(c -> c.getFeature() == Feature.CALCULATION_HOSPITAL).findFirst()
+                .get().getRight()).isEqualTo(Right.Deny);
     }
 
     @Test
-    public void ensureRightsForNonAccountFeatureTest() {
+    public void ensureRightsForAccountFeatureWithNonExistsRightsAndNoIkAdminTest() {
+        Account acc = createNewAccount(2);
+        acc.addFeature(Feature.NUB, true);
+        acc.addFeature(Feature.CALCULATION_HOSPITAL, true);
+        int ik = 222222222;
+        List<Feature> features1 = new ArrayList<>();
+        features1.add(Feature.NUB);
 
+        List<IkAdmin> ikAdminsForIk = new ArrayList<>();
+        ikAdminsForIk.add(createNewIkAdmin(createNewAccount(1), ik, features1));
+
+        Set<Integer> ikAdminsForEmailing = new HashSet<>();
+
+        AccessRightHelper.ensureRightsForAccountFeature(acc, ikAdminsForIk, ikAdminsForEmailing, ik);
+
+        Assertions.assertThat(acc.getAccessRights()).hasSize(1);
+        Assertions.assertThat(acc.getAccessRights().stream().filter(c -> c.getFeature() == Feature.NUB).findFirst()
+                .get().getRight()).isEqualTo(Right.Deny);
+    }
+
+
+
+    @Test
+    public void ensureRightsForNonAccountFeatureWithFeatuerWithIkAdminTest() {
+        Account acc = createNewAccount(2);
+        acc.addFeature(Feature.NUB, true);
+        int ik = 222222222;
+        List<Feature> features1 = new ArrayList<>();
+        features1.add(Feature.CARE);
+
+        List<IkAdmin> ikAdminsForIk = new ArrayList<>();
+        ikAdminsForIk.add(createNewIkAdmin(createNewAccount(1), ik, features1));
+
+        Set<Integer> ikAdminsForEmailing = new HashSet<>();
+
+        AccessRightHelper.ensureRightsForNonAccountFeature(acc, ikAdminsForIk, ikAdminsForEmailing, ik);
+
+        Assertions.assertThat(acc.getAccessRights()).hasSize(1);
+        Assertions.assertThat(acc.getAccessRights().stream().filter(c -> c.getFeature() == Feature.CARE).findFirst()
+                .get().getRight()).isEqualTo(Right.Deny);
+        Assertions.assertThat(acc.getFeatures().stream().anyMatch(c -> c.getFeature() == Feature.CARE)).isTrue();
     }
 
     @Test
-    public IkAdmin createNewIkAdmin(Account account, int ik, List<Feature> features) {
+    public void ensureRightsForNonAccountFeatureWithFeatuerWithNoIkAdminTest() {
+        Account acc = createNewAccount(2);
+        acc.addFeature(Feature.NUB, true);
+        int ik = 222222222;
+
+        List<IkAdmin> ikAdminsForIk = new ArrayList<>();
+
+        Set<Integer> ikAdminsForEmailing = new HashSet<>();
+
+        AccessRightHelper.ensureRightsForNonAccountFeature(acc, ikAdminsForIk, ikAdminsForEmailing, ik);
+
+        Assertions.assertThat(acc.getAccessRights()).hasSize(0);
+        Assertions.assertThat(acc.getFeatures().stream().noneMatch(c -> c.getFeature() == Feature.CARE)).isTrue();
+    }
+
+    private IkAdmin createNewIkAdmin(Account account, int ik, List<Feature> features) {
         return new IkAdmin(account.getId(), ik, "", features);
+    }
+
+    private Account createNewAccount(int id) {
+        Account acc = new Account();
+        acc.setId(id);
+        return acc;
     }
 
 
