69310d9c73eba1128bb5f8eca19baa7ebd4c8fce
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2019-01-24 10:21:14 +0100

    #161 not ignore upper an lower case chars

--- DataPortalInsurance/src/main/java/org/inek/dataportal/insurance/care/facade/CareSignatureCheckerFacade.java
+++ DataPortalInsurance/src/main/java/org/inek/dataportal/insurance/care/facade/CareSignatureCheckerFacade.java
@@ -1,23 +1,24 @@
 package org.inek.dataportal.insurance.care.facade;
 
-import java.util.List;
 import org.inek.dataportal.common.data.AbstractDataAccess;
+import org.inek.dataportal.insurance.care.backingbean.SignatureEntry;
 
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
-import org.inek.dataportal.insurance.care.backingbean.SignatureEntry;
+import java.util.List;
 
 @RequestScoped
 @Transactional
 public class CareSignatureCheckerFacade extends AbstractDataAccess {
 
     public SignatureEntry retrieveInformationForSignature(String signature) {
-        String sql = "select bi.prbiSignature, bi.prbiIk, cu.cuName, bi.prbiYear, bi.prbiQuarter\n"
-                + "from care.ProofRegulationBaseInformation bi\n"
-                + "join CallCenterDB.dbo.ccCustomer cu on cu.cuIK = bi.prbiIk\n"
-                + "where prbiStatusId = 10\n"
-                + "and bi.prbiSignature = ?";
+        String sql = "select bi.prbiSignature, bi.prbiIk, cu.cuName, bi.prbiYear, bi.prbiQuarter\n" +
+                "from care.ProofRegulationBaseInformation bi\n" +
+                "join CallCenterDB.dbo.ccCustomer cu on cu.cuIK = bi.prbiIk\n" +
+                "where prbiStatusId = 10\n" +
+                "and bi.prbiSignature = ? \n" +
+                "COLLATE SQL_Latin1_General_Cp1_CS_AS";
         Query query = getEntityManager().createNativeQuery(sql);
         query.setParameter(1, signature);
         List resultList = query.getResultList();
