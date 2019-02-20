c86b9f6a87f979a8c505ce720ae537b25838b07e
Michael Müller <michael.mueller@inek-drg.de>
2019-02-20 10:55:39 +0100

    move test to CI

--- DataPortalCare/src/test/java/org/inek/dataportal/care/utils/CI_CareSignatureCreaterTest.java
+++ DataPortalCare/src/test/java/org/inek/dataportal/care/utils/CI_CareSignatureCreaterTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-class CareSignatureCreaterTest {
+class CI_CareSignatureCreaterTest {
 
     @Test
     void createSignatureTest() {
