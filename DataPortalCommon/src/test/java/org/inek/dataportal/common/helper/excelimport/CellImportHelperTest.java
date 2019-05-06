7000d8b66be2387a922b523afcca17601c18abcd
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2019-05-06 12:06:01 +0200

    refactor test

--- DataPortalCommon/src/test/java/org/inek/dataportal/common/helper/excelimport/CellImportHelperTest.java
+++ DataPortalCommon/src/test/java/org/inek/dataportal/common/helper/excelimport/CellImportHelperTest.java
@@ -14,305 +14,199 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-
 
 class CellImportHelperTest {
 
     @Test
     void getStringFromCellWithNullTest() {
-        try {
+        Assertions.assertThatCode(() -> {
             Assertions.assertThat(CellImportHelper.getStringFromCell(null, true)).isEqualTo("");
-        } catch (Exception ex) {
-            Assertions.assertThat(true).isFalse();
-        }
+        }).doesNotThrowAnyException();
     }
 
     @Test
     void getStringFromCellWithFormulaTest() {
         Cell newCell = createNewCell();
-
         newCell.setCellType(CellType.FORMULA);
 
-        try {
-            Assertions.assertThat(CellImportHelper.getStringFromCell(newCell)).isEqualTo("Test");
-        } catch (Exception ex) {
-            Assertions.assertThat(ex).isExactlyInstanceOf(FormulaInCellException.class);
-            Assertions.assertThat(((FormulaInCellException) ex).getCell()).isEqualTo(newCell);
-        }
-    }
+        Throwable thrown = Assertions.catchThrowable(() -> {
+            CellImportHelper.getStringFromCell(newCell);
+        });
 
-    @Test
-    void getStringFromCellWithValidStringsTest() {
-        List<Tuple<Cell, String>> values = new ArrayList<Tuple<Cell, String>>();
-
-        values.add(new Tuple<Cell, String>(createNewCell("Test"), "Test"));
-        values.add(new Tuple<Cell, String>(createNewCell(" Test"), " Test"));
-        values.add(new Tuple<Cell, String>(createNewCell("T e s t"), "T e s t"));
-        values.add(new Tuple<Cell, String>(createNewCell("1234"), "1234"));
-        values.add(new Tuple<Cell, String>(createNewCell("0123"), "0123"));
-        values.add(new Tuple<Cell, String>(createNewCell("01"), "01"));
-        values.add(new Tuple<Cell, String>(createNewCell("äöü,.;'"), "äöü,.;'"));
-        values.add(new Tuple<Cell, String>(createNewCell("Testsatz: \"dasd\"."), "Testsatz: \"dasd\"."));
-        values.add(new Tuple<Cell, String>(createNewCell(""), ""));
-
-
-        for (Tuple<Cell, String> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getStringFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
+        Assertions.assertThat(thrown).isInstanceOf(FormulaInCellException.class);
+        Assertions.assertThat(((FormulaInCellException) thrown).getCell()).isEqualTo(newCell);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Test", " Test", "T e s t", "1234", "0123", "01", "äöü,.;'", "Testsatz: \"dasd\".", "", "123.00"})
     void getStringFromCellWithValidStringsTest(String cellValue) {
-        Cell cell = createNewCell(cellValue);
-        try {
-            Assertions.assertThat(CellImportHelper.getStringFromCell(cell)).isEqualTo(cellValue);
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
+        Cell newCell = createNewCell(cellValue);
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getStringFromCell(newCell)).isEqualTo(cellValue);
+        }).doesNotThrowAnyException();
     }
 
-    @Test
-    void getStringFromCellWithIntegerNumberTest() {
-        List<Tuple<Cell, String>> values = new ArrayList<Tuple<Cell, String>>();
-
-        values.add(new Tuple<Cell, String>(createNewCell(2), "2"));
-        values.add(new Tuple<Cell, String>(createNewCell(12), "12"));
-        values.add(new Tuple<Cell, String>(createNewCell(1224568.00), "1224568"));
-
-        for (Tuple<Cell, String> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getStringFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
-    }
-
-    @Test
-    void getStringFromCellWithDoubleNumberTest() {
-        List<Tuple<Cell, String>> values = new ArrayList<Tuple<Cell, String>>();
-
-        values.add(new Tuple<Cell, String>(createNewCell(2.20), "2.2"));
-        values.add(new Tuple<Cell, String>(createNewCell(12.235), "12.235"));
-        values.add(new Tuple<Cell, String>(createNewCell(1224568.001), "1224568.001"));
-
-        for (Tuple<Cell, String> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getStringFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
-    }
 
     @ParameterizedTest
     @CsvSource({
             "2, 2",
             "12, 12",
-            "1224568.00, 1224568",
+            "12245680, 12245680"
+    })
+    void getStringFromCellWithIntegerNumberTest(int cellValue, String expected) {
+        Cell newCell = createNewCell(cellValue);
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getStringFromCell(newCell)).isEqualTo(expected);
+        }).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
             "2.20, 2.2",
             "12.235, 12.235",
             "1224568.001, 1224568.001"
     })
-    void getStringFromCellWithValidNumbersTest(double cellValue, String expected) {
-        Cell cell = createNewCell(cellValue);
-        try {
-            Assertions.assertThat(CellImportHelper.getStringFromCell(cell)).isEqualTo(expected);
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
+    void getStringFromCellWithDoubleNumberTest(double cellValue, String expected) {
+        Cell newCell = createNewCell(cellValue);
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getStringFromCell(newCell)).isEqualTo(expected);
+        }).doesNotThrowAnyException();
     }
 
     @Test
     void getIntegerFromCellWithNullTest() {
-        try {
+        Assertions.assertThatCode(() -> {
             Assertions.assertThat(CellImportHelper.getIntegerFromCell(null, true, true)).isEqualTo(0);
-        } catch (Exception ex) {
-            Assertions.assertThat(true).isFalse();
-        }
+        }).doesNotThrowAnyException();
     }
 
     @Test
     void getIntegerFromCellWithFormulaTest() {
         Cell newCell = createNewCell();
-
         newCell.setCellType(CellType.FORMULA);
 
-        try {
-            Assertions.assertThat(CellImportHelper.getIntegerFromCell(newCell)).isEqualTo(1);
-        } catch (Exception ex) {
-            Assertions.assertThat(ex).isExactlyInstanceOf(FormulaInCellException.class);
-            Assertions.assertThat(((FormulaInCellException) ex).getCell()).isEqualTo(newCell);
-        }
+        Throwable thrown = Assertions.catchThrowable(() -> {
+            CellImportHelper.getIntegerFromCell(newCell);
+        });
+
+        Assertions.assertThat(thrown).isInstanceOf(FormulaInCellException.class);
+        Assertions.assertThat(((FormulaInCellException) thrown).getCell()).isEqualTo(newCell);
     }
 
-    @Test
-    void getIntegerFromCellWithValidIntegersWithoutAcceptDoubleTest() {
-        List<Tuple<Cell, Integer>> values = new ArrayList<Tuple<Cell, Integer>>();
-
-        values.add(new Tuple<Cell, Integer>(createNewCell(1), 1));
-        values.add(new Tuple<Cell, Integer>(createNewCell(2345), 2345));
-        values.add(new Tuple<Cell, Integer>(createNewCell(100000), 100000));
-        values.add(new Tuple<Cell, Integer>(createNewCell(999999999), 999999999));
-
-
-        for (Tuple<Cell, Integer> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getIntegerFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
-
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2345, 100000, 999999999})
+    void getIntegerFromCellWithValidIntegersWithoutAcceptDoubleTest(int cellValue) {
+        Cell newCell = createNewCell(cellValue);
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getIntegerFromCell(newCell)).isEqualTo(cellValue);
+        }).doesNotThrowAnyException();
     }
 
-    @Test
-    void getIntegerFromCellWithDoublesWithoutAcceptDoubleTest() {
-        List<Tuple<Cell, Integer>> values = new ArrayList<Tuple<Cell, Integer>>();
+    @ParameterizedTest
+    @ValueSource(doubles = {2.20, 12.235, 1224568.001})
+    void getIntegerFromCellWithDoublesWithoutAcceptDoubleTest(double cellValue) {
+        Cell newCell = createNewCell(cellValue);
 
-        values.add(new Tuple<Cell, Integer>(createNewCell(1.10), 1111));
-        values.add(new Tuple<Cell, Integer>(createNewCell(2345.10), 2111345));
+        Throwable thrown = Assertions.catchThrowable(() -> {
+            CellImportHelper.getIntegerFromCell(newCell, false, false);
+        });
 
-        for (Tuple<Cell, Integer> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getIntegerFromCell(pair.cell, false, false)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(ex).isExactlyInstanceOf(IntegerInDoubleCellException.class);
-                Assertions.assertThat(((IntegerInDoubleCellException) ex).getCell()).isEqualTo(pair.cell);
-            }
-        }
+        Assertions.assertThat(thrown).isInstanceOf(IntegerInDoubleCellException.class);
+        Assertions.assertThat(((IntegerInDoubleCellException) thrown).getCell()).isEqualTo(newCell);
     }
 
-    @Test
-    void getIntegerFromCellWithDoublesWithtAcceptDoubleTest() {
-        List<Tuple<Cell, Integer>> values = new ArrayList<Tuple<Cell, Integer>>();
-
-        values.add(new Tuple<Cell, Integer>(createNewCell(1.10), 1));
-        values.add(new Tuple<Cell, Integer>(createNewCell(2345.00), 2345));
-        values.add(new Tuple<Cell, Integer>(createNewCell(2345.216584), 2345));
-        values.add(new Tuple<Cell, Integer>(createNewCell(2345.001), 2345));
-
-        for (Tuple<Cell, Integer> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getIntegerFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
+    @ParameterizedTest
+    @CsvSource({
+            "1.10, 1",
+            "2345.00, 2345",
+            "2345.216584, 2345",
+            "2345.001, 2345"
+    })
+    void getIntegerFromCellWithDoublesWithAcceptDoubleTest(double cellValue, int expected) {
+        Cell newCell = createNewCell(cellValue);
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getIntegerFromCell(newCell, true, true)).isEqualTo(expected);
+        }).doesNotThrowAnyException();
     }
 
-    @Test
-    void getIntegerFromCellWithStringValuesTest() {
-        List<Tuple<Cell, Integer>> values = new ArrayList<Tuple<Cell, Integer>>();
+    @ParameterizedTest
+    @ValueSource(strings = {"1.10", "", "Testen"})
+    void getIntegerFromCellWithStringValuesTest(String cellValue) {
+        Cell newCell = createNewCell(cellValue);
 
-        values.add(new Tuple<Cell, Integer>(createNewCell("1.10"), 104455145));
-        values.add(new Tuple<Cell, Integer>(createNewCell("Testen"), 2345));
+        Throwable thrown = Assertions.catchThrowable(() -> {
+            CellImportHelper.getIntegerFromCell(newCell, false, false);
+        });
 
-        for (Tuple<Cell, Integer> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getIntegerFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(ex).isExactlyInstanceOf(StringInNumericCellException.class);
-                Assertions.assertThat(((StringInNumericCellException) ex).getCell()).isEqualTo(pair.cell);
-            }
-        }
+        Assertions.assertThat(thrown).isInstanceOf(StringInNumericCellException.class);
+        Assertions.assertThat(((StringInNumericCellException) thrown).getCell()).isEqualTo(newCell);
     }
 
     @Test
     void getDoubleFromCellWithNullTest() {
-        try {
+        Assertions.assertThatCode(() -> {
             Assertions.assertThat(CellImportHelper.getDoubleFromCell(null, true)).isEqualTo(0);
-        } catch (Exception ex) {
-            Assertions.assertThat(true).isFalse();
-        }
+        }).doesNotThrowAnyException();
     }
 
     @Test
     void getDoubleFromCellWithFormulaTest() {
         Cell newCell = createNewCell();
-
         newCell.setCellType(CellType.FORMULA);
 
-        try {
-            Assertions.assertThat(CellImportHelper.getDoubleFromCell(newCell)).isEqualTo(1);
-        } catch (Exception ex) {
-            Assertions.assertThat(ex).isExactlyInstanceOf(FormulaInCellException.class);
-            Assertions.assertThat(((FormulaInCellException) ex).getCell()).isEqualTo(newCell);
-        }
+        Throwable thrown = Assertions.catchThrowable(() -> {
+            CellImportHelper.getDoubleFromCell(newCell);
+        });
+
+        Assertions.assertThat(thrown).isInstanceOf(FormulaInCellException.class);
+        Assertions.assertThat(((FormulaInCellException) thrown).getCell()).isEqualTo(newCell);
     }
 
-    @Test
-    void getDoubleFromCellWithValidDoublesTest() {
-        List<Tuple<Cell, Double>> values = new ArrayList<Tuple<Cell, Double>>();
-
-        values.add(new Tuple<Cell, Double>(createNewCell(1.0), 1.0));
-        values.add(new Tuple<Cell, Double>(createNewCell(0.2220), 0.222));
-        values.add(new Tuple<Cell, Double>(createNewCell(1.12351), 1.12351));
-
-        for (Tuple<Cell, Double> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getDoubleFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
+    @ParameterizedTest
+    @CsvSource({
+            "1.0, 1.0",
+            "0.2220, 0.222",
+            "1.12351, 1.12351"
+    })
+    void getDoubleFromCellWithValidDoublesTest(double cellValue, double expected) {
+        Cell newCell = createNewCell(cellValue);
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getDoubleFromCell(newCell)).isEqualTo(expected);
+        }).doesNotThrowAnyException();
     }
 
-    @Test
-    void getDoubleFromCellWithValidIntegersTest() {
-        List<Tuple<Cell, Double>> values = new ArrayList<Tuple<Cell, Double>>();
-
-        values.add(new Tuple<Cell, Double>(createNewCell(1), 1.0));
-        values.add(new Tuple<Cell, Double>(createNewCell(20), 20.0));
-        values.add(new Tuple<Cell, Double>(createNewCell(21534), 21534.0));
-
-        for (Tuple<Cell, Double> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getDoubleFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1.0",
+            "20, 20.0",
+            "21534, 21534.0"
+    })
+    void getDoubleFromCellWithValidIntegersTest(int cellValue, double expected) {
+        Cell newCell = createNewCell(cellValue);
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getDoubleFromCell(newCell)).isEqualTo(expected);
+        }).doesNotThrowAnyException();
     }
 
-    @Test
-    void getDoubleFromCellWithStringValuesTest() {
-        List<Tuple<Cell, Double>> values = new ArrayList<Tuple<Cell, Double>>();
+    @ParameterizedTest
+    @ValueSource(strings = {"1.10", "Testen"})
+    void getDoubleFromCellWithStringValuesTest(String cellValue) {
+        Cell newCell = createNewCell(cellValue);
 
-        values.add(new Tuple<Cell, Double>(createNewCell("1.10"), 104455145.0));
-        values.add(new Tuple<Cell, Double>(createNewCell("Testen"), 2345.0));
+        Throwable thrown = Assertions.catchThrowable(() -> {
+            CellImportHelper.getDoubleFromCell(newCell );
+        });
 
-        for (Tuple<Cell, Double> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getDoubleFromCell(pair.cell)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(ex).isExactlyInstanceOf(StringInNumericCellException.class);
-                Assertions.assertThat(((StringInNumericCellException) ex).getCell()).isEqualTo(pair.cell);
-            }
-        }
+        Assertions.assertThat(thrown).isInstanceOf(StringInNumericCellException.class);
+        Assertions.assertThat(((StringInNumericCellException) thrown).getCell()).isEqualTo(newCell);
     }
 
     @Test
     void getDoubleFromCellWithEmptyStringValuesTest() {
-        List<Tuple<Cell, Double>> values = new ArrayList<Tuple<Cell, Double>>();
-
-        values.add(new Tuple<Cell, Double>(createNewCell(""), 0.0));
-        values.add(new Tuple<Cell, Double>(null, 0.0));
-
-        for (Tuple<Cell, Double> pair : values) {
-            try {
-                Assertions.assertThat(CellImportHelper.getDoubleFromCell(pair.cell, true)).isEqualTo(pair.expected);
-            } catch (Exception ex) {
-                Assertions.assertThat(true).isFalse();
-            }
-        }
+        Assertions.assertThatCode(() -> {
+            Assertions.assertThat(CellImportHelper.getDoubleFromCell(createNewCell(""), true)).isEqualTo(0.0);
+            Assertions.assertThat(CellImportHelper.getDoubleFromCell(createNewCell(null), true)).isEqualTo(0.0);
+        }).doesNotThrowAnyException();
     }
 
 
@@ -339,15 +233,5 @@ class CellImportHelperTest {
         Cell cell = createNewCell();
         cell.setCellValue(value);
         return cell;
-    }
-
-    private class Tuple<X, Y> {
-        public final X cell;
-        public final Y expected;
-
-        public Tuple(X cell, Y expected) {
-            this.cell = cell;
-            this.expected = expected;
-        }
     }
 }
