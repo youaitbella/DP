7b6bdab81d63bf79e5ca948c94004f9c86d4baa4
Tim Lautentschläger <tim.lautenschlaeger@inek-drg.de>
2019-07-30 08:32:02 +0200

    service wip

--- PsyEvaluationService/src/main/java/org/inek/psyEvaluationService/backingBean/EvaluationServiceBean.java
+++ PsyEvaluationService/src/main/java/org/inek/psyEvaluationService/backingBean/EvaluationServiceBean.java
@@ -7,10 +7,14 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ManagedBean(name = "evaluationServiceBean")
 @ApplicationScoped
 public class EvaluationServiceBean implements Serializable {
+
+    private static final Logger LOGGER = Logger.getLogger(EvaluationServiceBean.class.toString());
 
     @Inject
     private AEBFacade _aebFacade;
@@ -25,6 +29,17 @@ public class EvaluationServiceBean implements Serializable {
 
     public void switchScannerEnable() {
         _scanEnable = !_scanEnable;
+    }
+
+    private void startWorking() {
+        while (true) {
+            LOGGER.log(Level.INFO, "TestLog");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
