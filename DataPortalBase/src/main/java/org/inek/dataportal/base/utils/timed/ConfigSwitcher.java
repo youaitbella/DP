dc877d17509ea18ce740b951709597b1311f490e
Michael Müller <michael.mueller@inek-drg.de>
2019-09-12 09:34:25 +0200

    automate psy nub switches #285

--- DataPortalBase/src/main/java/org/inek/dataportal/base/utils/timed/ConfigSwitcher.java
+++ DataPortalBase/src/main/java/org/inek/dataportal/base/utils/timed/ConfigSwitcher.java
@@ -1,10 +1,11 @@
 package org.inek.dataportal.base.utils.timed;
 
+import org.inek.dataportal.common.data.access.ConfigFacade;
+import org.inek.dataportal.common.enums.ConfigKey;
+
 import javax.ejb.Schedule;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import org.inek.dataportal.common.enums.ConfigKey;
-import org.inek.dataportal.common.data.access.ConfigFacade;
 
 /**
  *
@@ -19,16 +20,20 @@ public class ConfigSwitcher {
     private void enableNub() {
         _configFacade.saveConfig(ConfigKey.IsNubCreateEnabled, true);
         _configFacade.saveConfig(ConfigKey.IsNubSendEnabled, true);
+        _configFacade.saveConfig(ConfigKey.IsPsyNubSendEnabled, true);
+        _configFacade.saveConfig(ConfigKey.IsPsyNubCreateEnabled, true);
     }
 
     @Schedule(month = "11", dayOfMonth = "3", hour = "0")
     private void disableCreateNub() {
         _configFacade.saveConfig(ConfigKey.IsNubCreateEnabled, false);
+        _configFacade.saveConfig(ConfigKey.IsPsyNubCreateEnabled, false);
     }
 
     @Schedule(month = "11", dayOfMonth = "4", hour = "0")
     private void disableSendNub() {
         _configFacade.saveConfig(ConfigKey.IsNubSendEnabled, false);
+        _configFacade.saveConfig(ConfigKey.IsPsyNubSendEnabled, false);
     }
 
     @Schedule(month = "4", dayOfMonth = "1", hour = "0")
