e5771ce9b0d01797489e9934038d0c86a723b82e
Marcel Palenga <Marcel.Palenga@inek-drg.de>
2019-12-10 16:57:50 +0100

    #328 fill converter

--- DataPortalCare/src/main/java/org/inek/dataportal/care/entities/StructuralChanges/StructuralChangesMarkerConverter.java
+++ DataPortalCare/src/main/java/org/inek/dataportal/care/entities/StructuralChanges/StructuralChangesMarkerConverter.java
@@ -1,4 +1,17 @@
 package org.inek.dataportal.care.entities.StructuralChanges;
 
-public class StructuralChangesMarkerConverter {
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = false)
+public class StructuralChangesMarkerConverter implements AttributeConverter<StructuralChangesMarker, Integer> {
+    @Override
+    public Integer convertToDatabaseColumn(StructuralChangesMarker structuralChangesMarker) {
+        return structuralChangesMarker.getId();
+    }
+
+    @Override
+    public StructuralChangesMarker convertToEntityAttribute(Integer id) {
+        return StructuralChangesMarker.fromId(id);
+    }
 }
