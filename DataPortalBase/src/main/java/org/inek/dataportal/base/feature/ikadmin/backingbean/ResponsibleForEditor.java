16c80fae6d11cd9fdc5fce644e8eac6e6540eaf6
Michael Müller <michael.mueller@inek-drg.de>
2019-12-12 09:18:11 +0100

    #336 prepare dedicated backing bean

---
+++ DataPortalBase/src/main/java/org/inek/dataportal/base/feature/ikadmin/backingbean/ResponsibleForEditor.java
@@ -0,0 +1,10 @@
+package org.inek.dataportal.base.feature.ikadmin.backingbean;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class ResponsibleForEditor {
+
+}
