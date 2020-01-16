89ce5e78483e847d8eed5611b5474635883837be
muellermi <Iwn, wseb?>
2020-01-16 13:58:04 +0100

    remove dup strings

--- DataPortalCommon/src/main/java/org/inek/dataportal/common/controller/DialogController.java
+++ DataPortalCommon/src/main/java/org/inek/dataportal/common/controller/DialogController.java
@@ -5,30 +5,32 @@ import org.primefaces.PrimeFaces;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-/**
- *
- * @author muellermi
- */
 public class DialogController{
 
+    public static final String ALERT = "swal('%s', '%s', '%s');";
+
+    private static void alert(String title, String message, String warning) {
+        PrimeFaces.current().executeScript(String.format(ALERT, title, message, warning));
+    }
+
     public static void showWarningDialog(String title, String message) {
-        PrimeFaces.current().executeScript("swal('" + title + "', '" + message + "', 'warning');");
+        alert(title, message, "warning");
     }
 
     public static void showInfoDialog(String title, String message) {
-        PrimeFaces.current().executeScript("swal('" + title + "', '" + message + "', 'info');");
+        alert(title, message, "info");
     }
 
     public static void showErrorDialog(String title, String message) {
-        PrimeFaces.current().executeScript("swal('" + title + "', '" + message + "', 'error');");
+        alert(title, message, "error");
     }
 
     public static void showSuccessDialog(String title, String message) {
-        PrimeFaces.current().executeScript("swal('" + title + "', '" + message + "', 'success');");
+        alert(title, message, "success");
     }
 
     public static void showAccessDeniedDialog() {
-        PrimeFaces.current().executeScript("swal('Zugriff Verweigert', 'Sie haben keine Zugriffsrechte auf diese Daten', 'error');");
+        alert("Zugriff Verweigert", "Sie haben keine Zugriffsrechte auf diese Daten", "error");
     }
 
     public static void showInfoMessage(String message) {
@@ -43,11 +45,6 @@ public class DialogController{
 
     public static void showSendDialog() {
         showSuccessDialog("Daten Gesendet", "Ihre Daten wurden erfolgreich an das InEK gesendet");
-    }
-
-    public static void showOldAlertMessage(String message) {
-        String script = "alert ('" + message.replace("\r\n", "\n").replace("\n", "\\r\\n") + "');";
-        PrimeFaces.current().executeScript(script);
     }
 
     public static void openDialogByName(String dialogWidgetVarName) {
