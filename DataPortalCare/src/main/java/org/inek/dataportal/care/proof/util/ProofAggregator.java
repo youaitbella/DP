e1c90262d7b36d645c2ab14f62c792cffe91b460
muellermi <Iwn, wseb?>
2020-01-24 16:56:32 +0100

    #296 first, incomplete aggregation

--- DataPortalCare/src/main/java/org/inek/dataportal/care/proof/util/ProofAggregator.java
+++ DataPortalCare/src/main/java/org/inek/dataportal/care/proof/util/ProofAggregator.java
@@ -3,21 +3,43 @@ package org.inek.dataportal.care.proof.util;
 import org.inek.dataportal.care.entities.DeptWard;
 import org.inek.dataportal.care.proof.ProofWard;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class ProofAggregator {
 
     public static List<ProofWard> aggregateDeptWards(final List<DeptWard> wards, final Date validFrom, final Date validTo) {
-        List<ProofWard> proofWards = new ArrayList<>();
+        Map<String, ProofWard> proofWards = new HashMap<>();
         wards.stream()
                 .filter(w -> w.getValidFrom().compareTo(validFrom) <= 0)
                 .filter(w -> w.getValidTo().compareTo(validTo) >= 0)
-                .forEach(w -> {
-                    //proofWards.add()
+                .map(w -> {
+                    DeptWard ward = new DeptWard(w);
+                    ward.setValidFrom(w.getValidFrom().compareTo(validFrom) > 0 ? w.getValidFrom() : validFrom);
+                    ward.setValidTo(w.getValidTo().compareTo(validTo) < 0 ? w.getValidTo() : validTo);
+                    return ward;
+                })
+                .forEach(ward -> {
+                    String key = ward.getWardName() + "|" + ward.getLocationCodeVz();
+                    if (!proofWards.containsKey(key)) {
+                        ProofWard proofWard = ProofWard.builder()
+                                .wardName(ward.getWardName())
+                                .locationNumber(ward.getLocationCodeVz())
+                                .from(ward.getValidFrom())
+                                .to(ward.getValidTo())
+                                .addSensitiveArea(ward.getDept().getSensitiveArea())
+                                .addDept(ward.getFab())
+                                .addDeptName(ward.getDeptName())
+                                .beds(ward.getBedCount())
+                                .build();
+                        proofWards.put(key, proofWard);
+                    } else {
+                        ProofWard proofWard = proofWards.get(key);
+                        proofWard.addSensitiveArea(ward.getDept().getSensitiveArea());
+                        proofWard.addDept(ward.getFab());
+                        proofWard.addDeptName(ward.getDeptName());
+                    }
                 });
-        return proofWards;
+        return new ArrayList<>(proofWards.values());
     }
 
 
