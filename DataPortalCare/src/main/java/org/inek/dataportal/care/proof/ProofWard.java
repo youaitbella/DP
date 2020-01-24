05202ef5ae393cb92351df6b5c54b361ac883115
muellermi <Iwn, wseb?>
2020-01-24 11:25:47 +0100

    make ProofWard final

--- DataPortalCare/src/main/java/org/inek/dataportal/care/proof/ProofWard.java
+++ DataPortalCare/src/main/java/org/inek/dataportal/care/proof/ProofWard.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ProofWard {
+public final class ProofWard {
     public static Builder builder() {
         return new Builder();
     }
