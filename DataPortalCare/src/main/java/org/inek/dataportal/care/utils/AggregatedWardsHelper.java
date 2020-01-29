a61f577988e0d6fe626310374cef67396bbfca27
Marcel Palenga <Marcel.Palenga@inek-drg.de>
2020-01-29 13:25:38 +0100

    #296 refactor pair to FromToDate

--- DataPortalCare/src/main/java/org/inek/dataportal/care/utils/AggregatedWardsHelper.java
+++ DataPortalCare/src/main/java/org/inek/dataportal/care/utils/AggregatedWardsHelper.java
@@ -10,6 +10,7 @@ import org.inek.dataportal.common.helper.Utils;
 import org.inek.dataportal.common.mail.Mailer;
 import org.inek.dataportal.common.overall.ExceptionCollector;
 import org.inek.dataportal.common.utils.DateUtils;
+import org.inek.dataportal.common.utils.FromToDate;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -45,27 +46,27 @@ public class AggregatedWardsHelper {
                     fromDates.add(toDate);
                 });
 
-                List<Pair<Date, Date>> fromToDates = new ArrayList<>();
+                List<FromToDate> fromToDates = new ArrayList<>();
                 fromDates.stream().sorted(Date::compareTo).forEachOrdered(from -> {
                     toDates.stream().filter(toDate -> toDate.compareTo(from) >= 0).sorted(Date::compareTo).findFirst()
-                            .ifPresent(toDate -> fromToDates.add(new Pair<>(from, toDate)));
+                            .ifPresent(toDate -> fromToDates.add(new FromToDate(from, toDate)));
                 });
-                for (Pair<Date, Date> fromTo : fromToDates) {
+                for (FromToDate dates : fromToDates) {
                     deptWards.stream()
-                            .filter(deptWard -> deptWard.getValidFrom().compareTo(fromTo.getKey()) <= 0
-                                    && deptWard.getValidTo().compareTo(fromTo.getValue()) >= 0)
+                            .filter(deptWard -> deptWard.getValidFrom().compareTo(dates.from()) <= 0
+                                    && deptWard.getValidTo().compareTo(dates.to()) >= 0)
                             .sorted(Comparator.comparing(DeptWard::getFab)).forEach(deptWard -> {
                         String key = deptWard.getLocationCodeP21()
                                 //+ "|" + ward.getLocationCodeVz() for future usage
                                 + "|" + deptWard.getLocationText()
                                 + "|" + deptWard.getWardName().toLowerCase().replace(" ", "")
                                 // #332 + "|" + (deptWard.getDept().getDeptArea() == 3 ? "Intensiv" : "Other")
-                                + "|" + DateUtils.toAnsi(fromTo.getKey())
-                                + "|" + DateUtils.toAnsi(fromTo.getValue());
+                                + "|" + DateUtils.toAnsi(dates.from())
+                                + "|" + DateUtils.toAnsi(dates.to());
                         if (aggregatedWards.containsKey(key)) {
-                            aggregatedWards.get(key).aggregate(deptWard, fromTo.getKey(), fromTo.getValue());
+                            aggregatedWards.get(key).aggregate(deptWard, dates.from(), dates.to());
                         } else {
-                            aggregatedWards.put(key, new AggregatedWards(deptWard, fromTo.getKey(), fromTo.getValue()));
+                            aggregatedWards.put(key, new AggregatedWards(deptWard, dates.from(), dates.to()));
                         }
 
                     });
