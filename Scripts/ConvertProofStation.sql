3d7975aa58ebb6cf2f6736848f99cf123a2440ea
Michael Müller <michael.mueller@inek-drg.de>
2020-02-25 10:03:18 +0100

    #296 update script

--- Scripts/ConvertProofStation.sql
+++ Scripts/ConvertProofStation.sql
@@ -3,6 +3,8 @@ use DataPortalDev
 /*
 delete care.proof where prProofRegulationStationId = 0
 
+update care.proof set prProofWardId = 0
+
 delete care.proofWard where pwId > 0
 DBCC CHECKIDENT ([care.proofWard], reseed, 1)
 
@@ -14,99 +16,24 @@ select prsIk, 0, prsLocationCode, max(replace(prsStationName, '  ', ' '))
 from care.ProofRegulationStation
 where prsIk > 0
 group by prsIk, prsLocationCode, lower(replace(prsStationName, ' ', ''))
---group by prsIk, prsLocationCode, prsStationName
 
 --select * from care.ProofWard
 
-;
-with sensitiveRanking(sensitiveDomainId, seq) as (
-    select d.pbdSensitiveAreaId, row_number() over (order by d.pbdPpug, n.pbdPpug, d.pbdPart, n.pbdPart) as seq
-    from DataPortal.care.PpugBaseData d
-             join DataPortal.care.PpugBaseData n on d.pbdSensitiveAreaId = n.pbdSensitiveAreaId
-    where d.pbdShift = 1
-      and n.pbdShift = 0
-      and 2019 between d.pbdValidFrom and d.pbdValidTo
-      and 2019 between n.pbdValidFrom and n.pbdValidTo
-)
--- insert into care.proof (prProofRegulationBaseInformationId, prProofRegulationStationId, prProofWardId, prValidFrom, prValidTo, prMonth, prDeptNumbers, prDeptNames, prSensitiveDomains, prSensitiveDomainId, prShift, prBeds, prCountShift, prNurse, prHelpeNurse, prPatientOccupancy, prCountShiftNotRespected, prPatientPerNurse, prCountHelpeNurseChargeable, prComment)
-select prProofRegulationBaseInformationId,
-       0,
-       pwId,
-       CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01', 102)                                  as ValidFrom,
-       CONVERT(smalldatetime, EOMONTH(CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01',
-                                                             102)))                                              as ValidTo,
-       prMonth,
-       dbo.DistinctList(prsFabNumber)                                                                            as depts,
-       dbo.DistinctList(prsFabName)                                                                              as deptNames,
-       dbo.DistinctList(sdName)                                                                                  as SensitiveAreas,
-       min(seq * 100 + sensitiveDomainId) % 100                                                                  as SensitiveDomainId,
-       prShift,
-       prBeds,
-       prCountShift,
-       prNurse,
-       prHelpeNurse,
-       prPatientOccupancy,
-       prCountShiftNotRespected,
-       max(prPatientPerNurse),
-       min(prCountHelpeNurseChargeable),
-       prComment
-from care.Proof
-         join care.ProofRegulationStation on prProofRegulationStationId = prsId
-         join care.ProofWard on prsIk = pwIk and prsLocationCode = pwLocationText and
-                                lower(replace(prsStationName, ' ', '')) = lower(replace(pwName, ' ', ''))
-         join care.listSensitiveDomain on prsSensitiveAreaId = sdId
-         join sensitiveRanking on prsSensitiveAreaId = sensitiveDomainId
-where prProofRegulationStationId > 0
---group by prProofRegulationBaseInformationId, pwId, prMonth, prShift // eigentlich sollten die Daten auf dieser Ebene eindeutig sein, sind sie aber nicht.
--- durch die eindeutige Gruppierung ergeben sich zusätzliche Datensätze. Lieber ein paar zuviel, als fehlende Info
-group by prProofRegulationBaseInformationId, pwId, prMonth, prShift, prBeds, prCountShift, prNurse, prHelpeNurse,
-         prPatientOccupancy, prCountShiftNotRespected, prComment
+
 /*
--- variant 1: create new entry for each existing entry
--- insert into care.proof (prProofRegulationBaseInformationId, prProofRegulationStationId, prProofWardId, prValidFrom, prValidTo, prMonth, prDeptNumbers, prDeptNames, prSensitiveDomains, prSignificantSensitiveDomainId, prShift, prBeds, prCountShift, prNurse, prHelpeNurse, prPatientOccupancy, prCountShiftNotRespected, prPatientPerNurse, prCountHelpeNurseChargeable, prComment)
-select prProofRegulationBaseInformationId,
-       0,
-       pwId,
-       CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01', 102)                                  as ValidFrom,
-       CONVERT(smalldatetime, EOMONTH(CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01',
-                                                             102)))                                              as ValidTo,
-       prMonth,
-       prsFabNumber,
-       prsFabName,
-       sdName,
-       sdId,
-       prShift,
-       prBeds,
-       prCountShift,
-       prNurse,
-       prHelpeNurse,
-       prPatientOccupancy,
-       prCountShiftNotRespected,
-       prPatientPerNurse,
-       prCountHelpeNurseChargeable,
-       prComment
-from care.Proof
-         join care.ProofRegulationStation on prProofRegulationStationId = prsId
-         join care.ProofWard on prsIk = pwIk and prsLocationCode = pwLocationText and
-                                lower(replace(prsStationName, ' ', '')) = lower(replace(pwName, ' ', ''))
-         join care.listSensitiveDomain on prsSensitiveAreaId = sdId
-where prProofRegulationStationId > 0 and prsYear = 2019
- */
-
- /*
--- variant 2: update existing entries
+-- update existing entries
 update care.Proof set prProofWardId = pwId,
-                      prValidFrom = CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01', 102),
-                      prValidTo = CONVERT(smalldatetime, EOMONTH(CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01', 102))),
-                      prDeptNumbers = prsFabNumber,
-                      prDeptNames = prsFabName,
-                      prSensitiveDomains = sdName,
-                      prSignificantSensitiveDomainId = sdId
+                     prProofRegulationStationId = 0,
+                     prValidFrom = CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01', 102),
+                     prValidTo = CONVERT(smalldatetime, EOMONTH(CONVERT(smalldatetime, '2019-' + CONVERT(varchar, prMonth) + '-01', 102))),
+                     prDeptNumbers = prsFabNumber,
+                     prDeptNames = prsFabName,
+                     prSensitiveDomains = sdName,
+                     prSignificantSensitiveDomainId = sdId
 from care.Proof
-         join care.ProofRegulationStation on prProofRegulationStationId = prsId
-         join care.ProofWard on prsIk = pwIk and prsLocationCode = pwLocationText and
-                                lower(replace(prsStationName, ' ', '')) = lower(replace(pwName, ' ', ''))
-         join care.listSensitiveDomain on prsSensitiveAreaId = sdId
+        join care.ProofRegulationStation on prProofRegulationStationId = prsId
+        join care.ProofWard on prsIk = pwIk and prsLocationCode = pwLocationText and
+                               lower(replace(prsStationName, ' ', '')) = lower(replace(pwName, ' ', ''))
+        join care.listSensitiveDomain on prsSensitiveAreaId = sdId
 where prProofRegulationStationId > 0 and prsYear = 2019
-
- */
+*/
