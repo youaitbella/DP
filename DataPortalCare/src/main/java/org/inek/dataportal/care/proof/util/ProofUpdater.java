61e14b56db4d06cfc04073e7605e46946e908bd3
Michael Müller <michael.mueller@inek-drg.de>
2020-03-05 16:39:18 +0100

    #420 ProofUpdater *WIP*

--- DataPortalCare/src/main/java/org/inek/dataportal/care/proof/util/ProofUpdater.java
+++ DataPortalCare/src/main/java/org/inek/dataportal/care/proof/util/ProofUpdater.java
@@ -1,12 +1,18 @@
 package org.inek.dataportal.care.proof.util;
 
 import org.inek.dataportal.care.entities.DeptBaseInformation;
+import org.inek.dataportal.care.enums.Shift;
 import org.inek.dataportal.care.proof.ProofFacade;
+import org.inek.dataportal.care.proof.ProofWardInfo;
 import org.inek.dataportal.care.proof.entity.ProofRegulationBaseInformation;
+import org.inek.dataportal.common.enums.WorkflowStatus;
+import org.inek.dataportal.common.utils.DateUtils;
+import org.inek.dataportal.common.utils.Period;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Dependent
 public class ProofUpdater {
@@ -30,7 +36,32 @@ public class ProofUpdater {
 
     }
 
-    private void updateProof(ProofRegulationBaseInformation proofBaseInfo, DeptBaseInformation deptBaseInformation) {
+    private void updateProof(ProofRegulationBaseInformation proofBaseInfo, DeptBaseInformation deptBaseInfo) {
+        ProofRegulationBaseInformation originalProofBaseInfo = new ProofRegulationBaseInformation(proofBaseInfo);
+        int year = proofBaseInfo.getYear();
+        int quarter = proofBaseInfo.getQuarter();
+        boolean changed = false;
+
+        for (int month = quarter * 3 - 2; month <= quarter * 3; month++) {
+            Period period = DateUtils.firstAndLastDayOfMonth(year, month);
+            List<ProofWardInfo> proofWardInfos = ProofAggregator.aggregateDeptWards(deptBaseInfo.obtainCurrentWards(), period.from(), period.to());
+            changed |= updateProof(proofBaseInfo, proofWardInfos);
+        }
+        if (!changed) {
+            return;
+        }
+        originalProofBaseInfo.setStatus(WorkflowStatus.Retired);
+        proofFacade.save(originalProofBaseInfo);
+        proofFacade.save(proofBaseInfo);
+    }
+
+    private boolean updateProof(ProofRegulationBaseInformation proofBaseInfo, List<ProofWardInfo> proofWardInfos) {
+        for (ProofWardInfo proofWardInfo : proofWardInfos) {
+            for (Shift shift : Shift.values()) {
+                // todo
+            }
+        }
+
     }
 
 }
