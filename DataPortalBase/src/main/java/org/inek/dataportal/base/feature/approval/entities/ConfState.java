b281b294b50cf723f000ae6243b121448708106e
Michael Müller <michael.mueller@inek-drg.de>
2020-03-09 09:35:31 +0100

    #399 +ConfState

---
+++ DataPortalBase/src/main/java/org/inek/dataportal/base/feature/approval/entities/ConfState.java
@@ -0,0 +1,52 @@
+package org.inek.dataportal.base.feature.approval.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "listConfState", schema = "conf")
+public class ConfState {
+    // <editor-fold defaultstate="collapsed" desc="Property Id">
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "csId")
+    private Integer id = -1;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Property Name">
+    @Column(name = "csName")
+    private String name = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    // </editor-fold>
+
+    //<editor-fold desc="equals / hasCode">
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConfState confType = (ConfState) o;
+        return id.equals(confType.id) &&
+                name.equals(confType.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+    //</editor-fold>
+}
